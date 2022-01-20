b76ddf54c7ea2ff89a3e5c4092cd6dadc67f42f1
JeanEstevesOSF <94859195+JeanEstevesOSF@users.noreply.github.com>
2022-01-20 15:55:52 -0300

    [task-1] reading csv dict list

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,12 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    rows = []
+    with open(path, encoding="utf8", newline="") as file:
+        csvreader = list(csv.DictReader(file))
+        for row in csvreader:
+            rows.append(row)
+    return rows
