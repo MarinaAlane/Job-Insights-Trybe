a8130c4f72d8a933e17eddf04e68130878c58df1
Jessica Arruda <sicarruda@gmail.com>
2022-01-20 19:30:35 -0300

    req 02

--- src/insights.py
+++ src/insights.py
@@ -1,19 +1,12 @@
+from .jobs import read
+
+
 def get_unique_job_types(path):
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    return []
+    data = read(path)
+    jobs_types = set()
+    for job_type in data['job_type']:
+        jobs_types.append(job_type)
+    return jobs_types
 
 
 def filter_by_job_type(jobs, job_type):
