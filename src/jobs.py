1448b5e2f04131689feb12565a7ea701f1fd04e1
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-01-20 16:09:36 -0300

    Add: Req01

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,19 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
+    with open(path) as file:
+        jobs_csv = csv.reader(file, delimiter=",", quotechar='"')
+        header, *rows = jobs_csv
 
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    # Para usar o .zip eu lembrei do platao das 13:00 de 20/01/2022
+    # e tambem consultei este artigo em RealPython para converter de HEX para Humanos
+    # https://realpython.com/python-zip-function/
+    content = []
+    for row in rows:
+        row = dict(zip(header, row))
+        content.append(row)
+    print(content[0])
+    return content
