32fb6073cced3ad5d55106cb43fe387c87786ba8
Hamander <1hamander@gmail.com>
2022-01-20 16:38:15 -0300

    build: jobs.py req1 complet

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,16 @@
+import csv
 from functools import lru_cache
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    with open(path, 'r') as file:
+        csv_reader = csv.reader(file, delimiter=",", quotechar='"')
+        header, *data = csv_reader
+        result = []
+        for row in data:
+            curr = dict()
+            for i in range(len(header)):
+                curr[header[i]] = row[i]
+            result.append(curr)
+    return result
