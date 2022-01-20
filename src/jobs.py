4e6630595f251219337cb8de275fdbbfe87f2a0b
Ghislaine <glenox79@gmail.com>
2022-01-20 17:17:48 -0300

    feature: jobs-py

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,20 @@
 from functools import lru_cache
+import csv
 
 
+# https://docs.python.org/3/library/csv.html
+# Uso do zip: plantão do dia 20 sobre estruturas de Dados e List Comprehension
+# ainda bem que tirei print para estudar!!!!
+# DICT-ZIP: https://pythonacademy.com.br/blog/dicts-ou-dicionarios-no-python
+# ZIP: http://devfuria.com.br/python/built-in-zip/
+# ZIP: https://pythonhelp.wordpress.com/2013/04/16/funcao-zip-em-python/
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    with open(path, newline='') as file:
+        reader_file = csv.reader(file, delimiter=",", quotechar='"')
+        header, *data = reader_file
+        jobs = []
+        for row in data:
+            row = dict(zip(header, row))
+            jobs.append(row)
+        return jobs
