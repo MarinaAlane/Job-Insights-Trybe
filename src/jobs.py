5c7308ee38db6815317537a3768c7b7b4a14760b
Pedro Piquini <piquini.pedro@gmail.com>
2022-01-20 14:58:35 -0300

    Iniciando o projeto

--- src/jobs.py
+++ src/jobs.py
@@ -1,8 +1,12 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
+    with open(path) as file:
+        jobs_data = csv.reader(file, delimiter=",", quotechar='"')
+        header, *data = jobs_data
     """Reads a file from a given path and returns its contents
 
     Parameters
