c4fbf3417dc4af9e3dc2b4a3c3b8c75a1824ad7d
CIGR2021 <appencigr@gmail.com>
2022-01-20 16:33:31 -0300

    Implementando Requisito 5

--- src/insights.py
+++ src/insights.py
@@ -70,21 +70,14 @@ def get_max_salary(path):
 
 
 def get_min_salary(path):
-    """Get the minimum salary of all jobs
+    min_salaries = set()
+    jobs_dict = read(path)
 
-    Must call `read`
+    for job in jobs_dict:
+        if job["min_salary"].isnumeric():
+            min_salaries.add(int(job["min_salary"]))
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    pass
+    return min(list(min_salaries))
 
 
 def matches_salary_range(job, salary):
