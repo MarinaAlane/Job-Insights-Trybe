4f2e9d539025c7d20cac543fced39c7a4f4e62a4
JeanEstevesOSF <94859195+JeanEstevesOSF@users.noreply.github.com>
2022-01-20 17:00:16 -0300

    [task-2] - get unique job types done

--- src/insights.py
+++ src/insights.py
@@ -1,19 +1,13 @@
+from src.jobs import read
+
+
 def get_unique_job_types(path):
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    return []
+    csv_list = read(path)
+    job_types = set()
+    for item in csv_list:
+        for job in item["job_type"].split(""):
+            job_types.add(job)
+    return job_types
 
 
 def filter_by_job_type(jobs, job_type):
