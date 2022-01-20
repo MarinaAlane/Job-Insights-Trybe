8ea7a4d41ab2be58b5087dcc8cba663fce74df97
CIGR2021 <appencigr@gmail.com>
2022-01-20 16:29:25 -0300

    Implementando Requisito 4

--- src/insights.py
+++ src/insights.py
@@ -59,21 +59,14 @@ def filter_by_industry(jobs, industry):
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
+    max_salaries = set()
+    jobs_dict = read(path)
 
-    Must call `read`
+    for job in jobs_dict:
+        if job["max_salary"].isnumeric():
+            max_salaries.add(int(job["max_salary"]))
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    pass
+    return max(list(max_salaries))
 
 
 def get_min_salary(path):
