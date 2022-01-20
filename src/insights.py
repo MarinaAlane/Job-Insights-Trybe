76e9d07d1712c1228a0559acf4f5deb6510a5f85
Pedro Piquini <piquini.pedro@gmail.com>
2022-01-20 19:04:33 -0300

    Req 6 completo

--- src/insights.py
+++ src/insights.py
@@ -41,7 +41,9 @@ def filter_by_job_type(jobs, job_type):
     list
         List of jobs with provided job_type
     """
-    return []
+    filtered_jobs = [job for job in jobs if job["job_type"] == job_type]
+    print(filtered_jobs)
+    return filtered_jobs
 
 
 def get_unique_industries(path):
