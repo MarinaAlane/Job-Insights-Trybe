0b7b828121cfdf1bd66411bcf5a35d9619c7122b
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2022-01-20 15:45:10 -0300

    feat: read csv file

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,14 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    with open(path) as file:
+        path_reader = csv.reader(file, delimiter=",", quotechar='"')
+        header, *data = path_reader
+        table = []
+        for row in data:
+            a = zip(header, row)
+            table.append(dict(a))
+    return table
