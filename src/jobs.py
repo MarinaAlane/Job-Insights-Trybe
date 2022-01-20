eb9c54d38d668fa2519e1d05a8564f6d9647da61
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2022-01-20 16:06:36 -0300

    cria funcao read  que le os dados de um arquivo csv

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,15 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its content
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    try:
+        with open(path) as file:
+            jobs_reader = csv.DictReader(file, delimiter=",", quotechar='"')
+            jobs = []
+            for row in jobs_reader:
+                jobs.append(row)
+            return jobs
+    except FileNotFoundError:
+        print("arquivo inexistente")
