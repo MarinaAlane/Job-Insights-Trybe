6c4b26830040862329abdc1b7615a998a12e3aa1
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2022-01-20 15:07:10 -0300

    adiciona requisito 03

--- src/insights.py
+++ src/insights.py
@@ -59,7 +59,15 @@ def get_unique_industries(path):
     list
         List of unique industries
     """
-    return []
+    all_jobs = jobs.read(path)
+
+    industries = set()
+
+    for job in all_jobs:
+        if job["industry"]:
+            industries.add(job["industry"])
+
+    return industries
 
 
 def filter_by_industry(jobs, industry):
