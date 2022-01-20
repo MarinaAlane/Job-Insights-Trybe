6677b61cbb8fac8d42fe61b0f2c2ee6e41bcf9ea
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2022-01-20 15:00:17 -0300

    adiciona requisito 02

--- src/insights.py
+++ src/insights.py
@@ -1,3 +1,6 @@
+from src import jobs
+
+
 def get_unique_job_types(path):
     """Checks all different job types and returns a list of them
 
@@ -13,7 +16,14 @@ def get_unique_job_types(path):
     list
         List of unique job types
     """
-    return []
+    all_jobs = jobs.read(path)
+
+    job_types = set()
+
+    for job in all_jobs:
+        job_types.add(job["job_type"])
+
+    return job_types
 
 
 def filter_by_job_type(jobs, job_type):
