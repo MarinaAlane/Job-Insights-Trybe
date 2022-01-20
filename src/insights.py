e4890c720ac12016d935c8430c93797422cab552
JeanEstevesOSF <94859195+JeanEstevesOSF@users.noreply.github.com>
2022-01-20 18:39:16 -0300

    [task-5] get min salary done

--- src/insights.py
+++ src/insights.py
@@ -70,21 +70,16 @@ def get_max_salary(path):
 
 
 def get_min_salary(path):
-    """Get the minimum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    pass
+    csv_list = read(path)
+    min_salary = 100000000
+    for item in csv_list:
+        if (
+            item["min_salary"] != ""
+            and item["min_salary"] != "invalid"
+            and int(item["min_salary"]) < min_salary
+        ):
+            min_salary = int(item["min_salary"])
+    return min_salary
 
 
 def matches_salary_range(job, salary):
