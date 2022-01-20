c84552ae5e9eebe89b74645a36e8f6b866fbd88e
Thyago Pessoa <flash_thyago@hotmail.com>
2022-01-20 15:21:20 -0300

    Req 1

--- src/jobs.py
+++ src/jobs.py
@@ -1,8 +1,15 @@
+import csv
 from functools import lru_cache
 
 
 @lru_cache
 def read(path):
+    new_arr = []
+    with open(path, 'r', newline='') as file:
+        read_file = list(csv.DictReader(file))
+        for row in read_file:
+            new_arr.append(row)
+    return new_arr
     """Reads a file from a given path and returns its contents
 
     Parameters
@@ -15,4 +22,4 @@ def read(path):
     list
         List of rows as dicts
     """
-    return []
+    # return []
