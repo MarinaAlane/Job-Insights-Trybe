48c2f6c3f0ea2a00052d8932042c7a1c8f4f3a8f
eraldoalmeida <eraldo.salmeida@gmail.com>
2022-01-20 17:22:35 -0300

    fix Req01

--- src/jobs.py
+++ src/jobs.py
@@ -4,5 +4,9 @@ import csv
 
 @lru_cache
 def read(path):
+    result = []
     with open(path) as file:
-        return list(csv.DictReader(file))
+        content = csv.DictReader(file, delimiter=",", quotechar='"')
+        for val in content:
+            result.append(val)
+    return result
