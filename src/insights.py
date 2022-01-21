8038a65afae08b45687557f82ce310a861b6fef8
Gustavo Miyagawa - Turma 11 <miyagawasantos@hotmail.com>
2022-01-21 18:10:36 -0400

    Cria lista de industria. Req. 3, feito.

--- src/insights.py
+++ src/insights.py
@@ -28,21 +28,15 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
+    jobs = read(path)
 
-    Must call `read`
+    industries = set()
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
+    for industry in jobs:
+        if industry["industry"] != "":
+            industries.add(industry["industry"])
 
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    return industries
 
 
 def filter_by_industry(jobs, industry):
