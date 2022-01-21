6786cbc8d8980b464f5c131146cac1ef3bc7e2a1
Gustavo Miyagawa - Turma 11 <miyagawasantos@hotmail.com>
2022-01-21 19:59:24 -0400

    Retorna uma lista com todos empregos onde a coluna industry corresponde ao parametro industry. Req. 7, feito.

--- src/insights.py
+++ src/insights.py
@@ -32,21 +32,13 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
+    filter_job = list()
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
+    for job in jobs:
+        if job["industry"] == industry:
+            filter_job.append(job)
 
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    return filter_job
 
 
 def get_max_salary(path):
