11b29fb382b691136acee2049a3d90a758b9c345
Hamander <1hamander@gmail.com>
2022-01-21 17:04:18 -0300

    build: sort_by test created

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,68 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
+    var = [{
+            "min_salary": 100,
+            "max_salary": 10000,
+            "date_posted": "2003-03-23"
+        },
+        {
+            "min_salary": 200,
+            "max_salary": 20000,
+            "date_posted": "2001-01-23"
+        },
+        {
+            "min_salary": 400,
+            "max_salary": 40000,
+            "date_posted": "2006-03-23"
+        }]
+    sort_by(var, "min_salary")
+    assert var == [{
+            "min_salary": 100,
+            "max_salary": 10000,
+            "date_posted": "2003-03-23"
+        },
+        {
+            "min_salary": 200,
+            "max_salary": 20000,
+            "date_posted": "2001-01-23"
+        },
+        {
+            "min_salary": 400,
+            "max_salary": 40000,
+            "date_posted": "2006-03-23"
+        }]
+    sort_by(var, "max_salary")
+    assert var == [{
+            "min_salary": 400,
+            "max_salary": 40000,
+            "date_posted": "2006-03-23"
+        },
+        {
+            "min_salary": 200,
+            "max_salary": 20000,
+            "date_posted": "2001-01-23"
+        },
+        {
+            "min_salary": 100,
+            "max_salary": 10000,
+            "date_posted": "2003-03-23"
+        }]
+    sort_by(var, "date_posted")
+    assert var == [{
+            "min_salary": 400,
+            "max_salary": 40000,
+            "date_posted": "2006-03-23"
+        },
+        {
+            "min_salary": 100,
+            "max_salary": 10000,
+            "date_posted": "2003-03-23"
+        },
+        {
+            "min_salary": 200,
+            "max_salary": 20000,
+            "date_posted": "2001-01-23"
+        }]
     pass
