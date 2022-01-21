4058c78031e5cf2294f23355256ba1822a1223f9
Marcelo <marsleite@gmail.com>
2022-01-20 22:06:11 -0300

    [feat] import csv, with open e return row for row

--- src/jobs.py
+++ src/jobs.py
@@ -1,19 +1,9 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    print(f"Reading {path}")
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    with open(path) as file:
+        reader = csv.DictReader(file, delimiter=",", quotechar='"')
+        return [row for row in reader]
