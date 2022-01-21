76ea74bb00707ba8f282ad8a4e7e9567caa2a47e
IsaacMagno <isaacmagno98@gmail.com>
2022-01-21 16:45:02 -0300

    desafio3

--- src/insights.py
+++ src/insights.py
@@ -31,21 +31,16 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
+    get_read = read(path)
+    unique_industries = []
 
-    Must call `read`
+    for res in get_read:
+        if res["industry"] \
+          not in unique_industries \
+          and len(res["industry"]) != 0:
+            unique_industries.append(res["industry"])
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    return unique_industries
 
 
 def filter_by_industry(jobs, industry):
