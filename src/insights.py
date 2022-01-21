4cae5fd1789cf1d22dbadceba0633f2dbfc40fb5
Hamander <1hamander@gmail.com>
2022-01-21 14:50:12 -0300

    build: insights filter_by_industry req 7 complete

--- src/insights.py
+++ src/insights.py
@@ -32,21 +32,11 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    filtered_industrys = []
+    for job in jobs:
+        if job["industry"] == industry:
+            filtered_industrys.append(job)
+    return filtered_industrys
 
 
 def get_max_salary(path):
