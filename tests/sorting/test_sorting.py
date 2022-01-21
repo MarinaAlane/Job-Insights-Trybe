1d14c22aed83f1efa4ad7cf117eb97825e9d55e6
Arthur Hermann <arthurhermannc@gmail.com>
2022-01-21 15:40:26 -0300

    req 10.1

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,10 @@
-# from src.sorting import sort_by
+import pytest
+from src.sorting import sort_by
 
 
-def test_sort_by_criteria():
-    pass
+def test_returns_typeerror_whith_unexpected_params():
+    with pytest.raises(
+        TypeError,
+        match="takes 2 positional arguments but 3 were given",
+    ):
+        sort_by([], "param2", "param3")
