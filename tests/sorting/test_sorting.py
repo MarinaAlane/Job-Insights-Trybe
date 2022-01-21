1821641ede46b52756340d77bbf0f5d3e3502edc
Filizzola <juliofilizzola@hotmail.com>
2022-01-21 16:47:00 -0300

    🚀: criando mock para response assert

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,19 +1,21 @@
 import pytest
 from src.sorting import sort_by
 from mock_test import mocked_jobs
-from mock_response_jobs import mock_response_jobs
+from mock_response_jobs import mock_response_jobs1
+from mock_response_jobs import mock_response_jobs2
+from mock_response_jobs import mock_response_jobs3
 
 
 def test_sort_by_criteria():
     pass
     sort_by(mocked_jobs, "min_salary")
-    assert mocked_jobs == mock_response_jobs.mock_response_jobs1
+    assert mocked_jobs == mock_response_jobs1
 
     sort_by(mocked_jobs, "max_salary")
-    assert mocked_jobs == mock_response_jobs.mock_response_jobs2
+    assert mocked_jobs == mock_response_jobs2
 
     sort_by(mocked_jobs, "date_posted")
-    assert mocked_jobs == mock_response_jobs.mock_response_jobs3
+    assert mocked_jobs == mock_response_jobs3
 
     with pytest.raises(ValueError):
         sort_by(mocked_jobs, "invalid_criteria")
