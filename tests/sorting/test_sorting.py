e09019c6172e25f57f3a3850bfdf24bcae2b6e6d
jhenriquemargrafm <jh.margraf@gmail.com>
2022-01-21 00:54:15 -0300

    Tentativa 2

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -4,7 +4,7 @@ from src.sorting import sort_by
 def test_sort_by_criteria():
     pass
     jobs = [
-        {'max_salary': 25, 'min_salary': 15, 'date_posted': '2020-12-01'},
+        {'max_salary': 25, 'min_salary': 15, 'date_posted': '2021-12-01'},
         {'max_salary': 50, 'min_salary': 2, 'date_posted': '2021-12-02'},
         {'max_salary': 100, 'min_salary': 10, 'date_posted': '2021-12-03'},
     ]
@@ -12,12 +12,12 @@ def test_sort_by_criteria():
     assert jobs == [
         {'max_salary': 100, 'min_salary': 10, 'date_posted': '2021-12-03'},
         {'max_salary': 50, 'min_salary': 2, 'date_posted': '2021-12-02'},
-        {'max_salary': 25, 'min_salary': 15, 'date_posted': '2020-12-01'},
+        {'max_salary': 25, 'min_salary': 15, 'date_posted': '2021-12-01'},
     ]
 
     sort_by(jobs, 'min_salary')
     assert jobs == [
         {'max_salary': 50, 'min_salary': 2, 'date_posted': '2021-12-02'},
         {'max_salary': 100, 'min_salary': 10, 'date_posted': '2021-12-03'},
-        {'max_salary': 25, 'min_salary': 15, 'date_posted': '2020-12-01'},
+        {'max_salary': 25, 'min_salary': 15, 'date_posted': '2021-12-01'},
     ]
