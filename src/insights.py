dec4fb10b3fe5b9ad23765aec7fcf98ec8e260b3
IsaacMagno <isaacmagno98@gmail.com>
2022-01-21 19:08:33 -0300

    desafio8

--- src/insights.py
+++ src/insights.py
@@ -1,5 +1,5 @@
-# from src.jobs import read
-from jobs import read
+from src.jobs import read
+# from jobs import read
 
 
 def get_unique_job_types(path):
@@ -75,29 +75,24 @@ def get_min_salary(path):
 
 
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
+    if "min_salary" not in job \
+      or "max_salary" not in job:
+        raise ValueError("invalid params")
 
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
+    min_salary = job["min_salary"]
+    max_salary = job["max_salary"]
 
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
+    if type(min_salary) != int \
+       or type(max_salary) != int:
+        raise ValueError("invalid type jobs")
 
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
+    if min_salary > max_salary:
+        raise ValueError("invalid salary range")
+
+    if type(salary) != int:
+        raise ValueError("invalid type salary")
+
+    return min_salary <= salary <= max_salary
 
 
 def filter_by_salary_range(jobs, salary):
