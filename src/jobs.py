5a4ee0011b6d48aacb4460c84e04ccbf3dac4ca1
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-01-21 20:20:46 -0300

    requisito 1

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,10 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file  from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    with open(path) as file:
+        dict_jobs_reader = csv.DictReader(file)
+        list_of_dict = list(dict_jobs_reader)
+        return list_of_dict
