6c773c940cdfce5eda7b7d09c3645c81f6aa36e0
Luciano <luciano@desrotulando.com>
2022-01-22 12:57:17 -0300

    req 3 - Concluido

--- src/insights.py
+++ src/insights.py
@@ -2,9 +2,9 @@ from src.jobs import read
 
 
 def get_unique_job_types(path):
-    list = read(path)
+    csv = read(path)
     job_type = set()
-    for row in list:
+    for row in csv:
         job_type.add(row["job_type"])
     return job_type
 
@@ -28,21 +28,11 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    csv = read(path)
+    list = set()
+    for item in csv:
+        list.add(item["industry"])
+    return list
 
 
 def filter_by_industry(jobs, industry):
