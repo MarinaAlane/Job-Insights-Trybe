f5b663d5fe38c6089725af1a9b3fef1cbd790b58
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-01-22 16:38:17 -0300

    refact requisito 10

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -2,43 +2,43 @@ import pytest
 from src.sorting import sort_by
 
 
-@pytest.fixture
-def jobs():
-    return [
-        {"min_salary": 100, "max_salary": 4500, "date_posted": "2021-12-05"},
-        {"min_salary": 300, "max_salary": 2800, "date_posted": "2019-01-06"},
-        {"min_salary": 500, "max_salary": 1000, "date_posted": "2020-11-11"}
+def test_sort_by_criteria():
+    jobs = [
+        {"min_salary": 100, "max_salary": 4500, "date_posted": "2021-01-01"},
+        {"min_salary": 300, "max_salary": 2800, "date_posted": "2019-01-01"},
+        {"min_salary": 500, "max_salary": 1000, "date_posted": "2020-01-01"},
+        {"min_salary": '', "max_salary": '', "date_posted": ''}
     ]
 
-
-sort_date_posted = [
-        {"min_salary": 100, "max_salary": 4500, "date_posted": "2021-12-05"},
-        {"min_salary": 500, "max_salary": 1000, "date_posted": "2020-11-11"},
-        {"min_salary": 300, "max_salary": 2800, "date_posted": "2019-01-06"}
+    sort_date_posted = [
+        {"min_salary": 100, "max_salary": 4500, "date_posted": "2021-01-01"},
+        {"min_salary": 500, "max_salary": 1000, "date_posted": "2020-01-01"},
+        {"min_salary": 300, "max_salary": 2800, "date_posted": "2019-01-01"},
+        {"min_salary": '', "max_salary": '', "date_posted": ''}
     ]
 
-sort_max_salary = [
-        {"min_salary": 100, "max_salary": 4500, "date_posted": "2021-12-05"},
-        {"min_salary": 300, "max_salary": 2800, "date_posted": "2019-01-06"},
-        {"min_salary": 500, "max_salary": 1000, "date_posted": "2020-11-11"}
+    sort_max_salary = [
+        {"min_salary": 100, "max_salary": 4500, "date_posted": "2021-12-01"},
+        {"min_salary": 300, "max_salary": 2800, "date_posted": "2019-01-01"},
+        {"min_salary": 500, "max_salary": 1000, "date_posted": "2020-11-01"},
+        {"min_salary": '', "max_salary": '', "date_posted": ''}
     ]
 
-sort_min_salary = [
-        {"min_salary": 100, "max_salary": 4500, "date_posted": "2021-12-05"},
-        {"min_salary": 300, "max_salary": 2800, "date_posted": "2019-01-06"},
-        {"min_salary": 500, "max_salary": 1000, "date_posted": "2020-11-11"}
+    sort_min_salary = [
+        {"min_salary": 100, "max_salary": 4500, "date_posted": "2021-01-01"},
+        {"min_salary": 300, "max_salary": 2800, "date_posted": "2019-01-01"},
+        {"min_salary": 500, "max_salary": 1000, "date_posted": "2020-01-01"},
+        {"min_salary": '', "max_salary": '', "date_posted": ''},
     ]
 
+    sort_by(jobs, 'min_salary')
+    assert jobs == sort_min_salary
 
-def test_sort_by_criteria(jobs):
-    sort_by(jobs, "date_posted")
-    assert jobs == sort_date_posted
-
-    sort_by(jobs, "max_salary")
+    sort_by(jobs, 'max_salary')
     assert jobs == sort_max_salary
 
-    sort_by(jobs, "min_salary")
-    assert jobs == sort_min_salary
+    sort_by(jobs, 'date_posted')
+    assert jobs == sort_date_posted
 
     invalid_criterias = ["job_type", "title"]
     for invalid_criteria in invalid_criterias:
