78e42553fff0570e36257b6e67362ac45e3f9505
Gabriela Feijo <feijo.gabriela@gmail.com>
2022-01-22 19:10:04 -0300

    add requirement 1

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,21 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
+    with open(path, "r") as file:
+        jobs_reader = csv.DictReader(file, delimiter=",", quotechar='"')
 
-    Parameters
-    ----------
-    path : str
-        Full path to file
+        result = []  # objeto não tem atributo append
+        for row in jobs_reader:
+            result.append(row)
+    return result
 
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+
+if __name__ == "__main__":  # identifica o arquivo que est sendo executado
+    print(read("src/jobs.csv"))
+
+# DictReader - leitor (pode ser escritor) baseado em dicionários.
+# não é preciso manipular os índices para acessar os dados das colunas.
+# https://docs.python.org/pt-br/3.6/library/csv.html
