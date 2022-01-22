639c1344ed3594a100e8638456102fe3f604854c
Paulo Henrique Assis <pieit@live.com>
2022-01-22 15:29:09 -0300

    Added req 10 tests

---
+++ tests/sorting/mocked_jobs.py
@@ -0,0 +1,38 @@
+job_sorted_by_min_salary = [
+    [
+        {"min_salary": 30, "max_salary": 10, "date_posted": "2019-02-01"},
+        {"min_salary": 10, "max_salary": 10, "date_posted": "2019-03-01"},
+        {"min_salary": 15, "max_salary": 10, "date_posted": "2019-04-01"},
+    ],
+    [
+        {"min_salary": 10, "max_salary": 10, "date_posted": "2019-03-01"},
+        {"min_salary": 15, "max_salary": 10, "date_posted": "2019-04-01"},
+        {"min_salary": 30, "max_salary": 10, "date_posted": "2019-02-01"},
+    ],
+]
+
+job_sorted_by_max_salary = [
+    [
+        {"min_salary": 10, "max_salary": 10, "date_posted": "2019-01-01"},
+        {"min_salary": 10, "max_salary": 15, "date_posted": "2019-01-01"},
+        {"min_salary": 10, "max_salary": 30, "date_posted": "2019-01-01"},
+    ],
+    [
+        {"min_salary": 10, "max_salary": 30, "date_posted": "2019-01-01"},
+        {"min_salary": 10, "max_salary": 15, "date_posted": "2019-01-01"},
+        {"min_salary": 10, "max_salary": 10, "date_posted": "2019-01-01"},
+    ],
+]
+
+job_sorted_by_date_posted = [
+    [
+        {"min_salary": 10, "max_salary": 10, "date_posted": "2019-06-06"},
+        {"min_salary": 10, "max_salary": 10, "date_posted": "2019-12-12"},
+        {"min_salary": 10, "max_salary": 10, "date_posted": "2019-01-01"},
+    ],
+    [
+        {"min_salary": 10, "max_salary": 10, "date_posted": "2019-12-12"},
+        {"min_salary": 10, "max_salary": 10, "date_posted": "2019-06-06"},
+        {"min_salary": 10, "max_salary": 10, "date_posted": "2019-01-01"},
+    ],
+]
--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,27 @@
-# from src.sorting import sort_by
+import pprint
+
+import pytest
+from src.sorting import sort_by
+
+from mocked_jobs import (
+    job_sorted_by_date_posted,
+    job_sorted_by_max_salary,
+    job_sorted_by_min_salary,
+)
+
+pp = pprint.PrettyPrinter(indent=4)
 
 
 def test_sort_by_criteria():
     pass
+    sort_by(job_sorted_by_min_salary[0], "min_salary")
+    assert job_sorted_by_min_salary[0] == job_sorted_by_min_salary[1]
+
+    sort_by(job_sorted_by_max_salary[0], "max_salary")
+    assert job_sorted_by_max_salary[0] == job_sorted_by_max_salary[1]
+
+    sort_by(job_sorted_by_date_posted[0], "date_posted")
+    assert job_sorted_by_date_posted[0] == job_sorted_by_date_posted[1]
+
+    with pytest.raises(ValueError):
+        sort_by(job_sorted_by_min_salary[0], "incorrect_criteria")
