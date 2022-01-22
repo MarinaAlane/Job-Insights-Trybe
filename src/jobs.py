79b8a6186b83d7d5bd6c706cc540ddf8f745a7d0
Luciano <luciano@desrotulando.com>
2022-01-22 10:26:38 -0300

    Req 1 - Concluido

--- src/jobs.py
+++ src/jobs.py
@@ -1,7 +1,13 @@
 from functools import lru_cache
-
+import csv
 
 @lru_cache
 def read(path):
+    my_list = []
+    with open(path, 'r') as file:
+        reader = csv.DictReader(file)
+
+        for row in reader:
+            my_list.append(row)
     
-    return []
+    return my_list
