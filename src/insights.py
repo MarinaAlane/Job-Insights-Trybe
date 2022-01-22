bdd533a2ad049f05474a80ec010cf7c6421ff25d
Rodrigo <rodrigopython16@gmail.com>
2022-01-22 14:07:01 -0300

    feature: done function to get min_salary

--- src/insights.py
+++ src/insights.py
@@ -72,21 +72,15 @@ def get_max_salary(path):
 
 
 def get_min_salary(path):
-    """Get the minimum salary of all jobs
+    informations = read(path)
+    only_salaries = list()
 
-    Must call `read`
+    for information in informations:
+        salary = information["min_salary"]
+        if salary.isdigit():
+            only_salaries.append(int(salary))
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    pass
+    return min(only_salaries)
 
 
 def matches_salary_range(job, salary):
