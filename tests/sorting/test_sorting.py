c2e0fb8a364bfa0aa2286f3eb0b2977648370a8b
Tarcisio-Menezes <tarcisioquim@gmail.com>
2022-01-22 19:19:13 -0300

    adiciona testes com assert

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,35 @@
-# from src.sorting import sort_by
+import pytest
+from src.sorting import sort_by
+
+fake_jobs = [
+    {"min_salary": 10, "max_salary": 20, "date_posted": "2022-01-22"},
+    {"min_salary": 20, "max_salary": 30, "date_posted": "2022-01-21"},
+    {"min_salary": 30, "max_salary": 40, "date_posted": "2022-01-20"},
+]
 
 
 def test_sort_by_criteria():
     pass
+    sort_by(fake_jobs, "min_salary")
+    assert fake_jobs == [
+        {"min_salary": 10, "max_salary": 20, "date_posted": "2022-01-22"},
+        {"min_salary": 20, "max_salary": 30, "date_posted": "2022-01-21"},
+        {"min_salary": 30, "max_salary": 40, "date_posted": "2022-01-20"},
+    ]
+
+    sort_by(fake_jobs, "max_salary")
+    assert fake_jobs == [
+        {"min_salary": 30, "max_salary": 40, "date_posted": "2022-01-20"},
+        {"min_salary": 20, "max_salary": 30, "date_posted": "2022-01-21"},
+        {"min_salary": 10, "max_salary": 20, "date_posted": "2022-01-22"},
+    ]
+
+    sort_by(fake_jobs, "date_posted")
+    assert fake_jobs == [
+        {"min_salary": 10, "max_salary": 20, "date_posted": "2022-01-22"},
+        {"min_salary": 20, "max_salary": 30, "date_posted": "2022-01-21"},
+        {"min_salary": 30, "max_salary": 40, "date_posted": "2022-01-20"},
+    ]
+
+    with pytest.raises(ValueError):
+        sort_by(fake_jobs, "invalid_criteria")
