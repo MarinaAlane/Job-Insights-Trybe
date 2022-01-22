627c840b93f2b1301bce35792562c694ff4ceed4
marlon307 <marlon-ramosb@hotmail.com>
2022-01-22 12:07:51 -0300

    remove comments

--- src/insights.py
+++ src/insights.py
@@ -10,21 +10,6 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
-
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
     return [j for j in jobs if j["job_type"] == job_type]
 
 
@@ -37,20 +22,6 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
     return [j for j in jobs if j["industry"] == industry]
 
 
