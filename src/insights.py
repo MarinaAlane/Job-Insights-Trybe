15cf670c1b688f6319a9a1035093c9818e7a891a
Gabriel Pimentel Fernandes <gabrielfernandessilva.ba@gmail.com>
2022-01-22 11:10:15 -0300

    Feature: Requisito 3

--- src/insights.py
+++ src/insights.py
@@ -29,21 +29,13 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    arr_csv = read(path)
+    uniqueIndustriesList = set()
+    for item in arr_csv:
+        industry = item["industry"]
+        if (industry != ''):
+            uniqueIndustriesList.add(industry)
+    return uniqueIndustriesList
 
 
 def filter_by_industry(jobs, industry):
