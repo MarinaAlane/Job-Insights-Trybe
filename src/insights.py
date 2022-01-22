3460c1343d5c3d6d0370c3e8040bef4a58b47bbb
Gabriela Feijo <feijo.gabriela@gmail.com>
2022-01-22 20:02:45 -0300

    add requirement 3

--- src/insights.py
+++ src/insights.py
@@ -59,7 +59,14 @@ def get_unique_industries(path):
     list
         List of unique industries
     """
-    return []
+    data = jobs.read(path)
+
+    industries = set()  # cria listas sem elementos duplicados
+    for industry in data:
+        if industry["industry"] != "":  # retira a linha em branco
+            industries.add(industry["industry"])
+
+    return industries
 
 
 def filter_by_industry(jobs, industry):
