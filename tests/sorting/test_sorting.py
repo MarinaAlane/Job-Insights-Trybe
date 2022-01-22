600285196bbecc36a7812613c6b9d2929a2f7c6d
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-01-22 15:25:06 -0300

    requisito 10

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -11,31 +11,35 @@ def jobs():
     ]
 
 
-def test_sort_by_criteria(jobs):
-    sort_date_posted = [
+sort_date_posted = [
         {"min_salary": 100, "max_salary": 4500, "date_posted": "2021-12-05"},
         {"min_salary": 500, "max_salary": 1000, "date_posted": "2020-11-11"},
         {"min_salary": 300, "max_salary": 2800, "date_posted": "2019-01-06"}
     ]
 
-    sort_max_salary = [
+sort_max_salary = [
         {"min_salary": 100, "max_salary": 4500, "date_posted": "2021-12-05"},
         {"min_salary": 300, "max_salary": 2800, "date_posted": "2019-01-06"},
         {"min_salary": 500, "max_salary": 1000, "date_posted": "2020-11-11"}
     ]
 
-    sort_min_salary = [
+sort_min_salary = [
         {"min_salary": 100, "max_salary": 4500, "date_posted": "2021-12-05"},
         {"min_salary": 300, "max_salary": 2800, "date_posted": "2019-01-06"},
         {"min_salary": 500, "max_salary": 1000, "date_posted": "2020-11-11"}
     ]
 
-    assert sort_by(jobs, "date_posted") == sort_date_posted
-    assert sort_by(jobs, "max_salary") == sort_max_salary
-    assert sort_by(jobs, "date_poster") == sort_min_salary
+
+def test_sort_by_criteria(jobs):
+    sort_by(jobs, "date_posted")
+    assert jobs == sort_date_posted
+
+    sort_by(jobs, "max_salary")
+    assert jobs == sort_max_salary
+
+    sort_by(jobs, "min_salary")
+    assert jobs == sort_min_salary
 
     invalid_criteria = "xablau"
-    with pytest.raises(
-        ValueError, match=f"invalid sorting criteria: {invalid_criteria}"
-    ):
+    with pytest.raises(ValueError):
         assert sort_by(jobs, invalid_criteria)
