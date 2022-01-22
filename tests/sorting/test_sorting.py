c502632546655424c409253f9806a76afefc241e
Gabriel Pimentel Fernandes <gabrielfernandessilva.ba@gmail.com>
2022-01-22 15:27:28 -0300

    Feature: Assert in Python - How to test

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -3,6 +3,7 @@ from src.sorting import sort_by
 
 def test_sort_by_criteria():
     pass
+    # https://realpython.com/python-testing/
     jobs = [
         {'max_salary': 10, 'min_salary': 5, 'date_posted': '2021-01-01'},
         {'max_salary': 20, 'min_salary': 1, 'date_posted': '2021-01-02'},
