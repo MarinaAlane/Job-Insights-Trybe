eaad2a92e870a3198bdb7202727a597226aa81d5
Tarcisio-Menezes <tarcisioquim@gmail.com>
2022-01-22 12:46:44 -0300

    add req. 3

--- src/insights.py
+++ src/insights.py
@@ -32,21 +32,15 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    try:
+        data = read(path)
+        industries = set()
+        for job in data:
+            if (job["industry"] != ""):
+                industries.add(job["industry"])
+        return industries
+    except FileNotFoundError:
+        print("file not found or corrupted")
 
 
 def filter_by_industry(jobs, industry):
