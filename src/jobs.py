6a8c7352958a24b55c02e47f038cc9cdf0cea166
Tarcisio-Menezes <tarcisioquim@gmail.com>
2022-01-22 11:57:37 -0300

    add req. 1

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,16 @@
+# https://python-forum.io/thread-9048.html
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    try:
+        with open(path) as file:
+            jobs_reader = csv.DictReader(file, delimiter=",", quotechar='"')
+            jobs = []
+            for job in jobs_reader:
+                jobs.append(job)
+            return jobs
+    except FileNotFoundError:
+        print("file not found or corrupted")
