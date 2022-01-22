3248eb2ea88c45e2e01ee5fdb5e539567b321936
Erick-Marinho <erickgaves@gmail.com>
2022-01-22 19:18:03 -0300

    Implementando funçãp para retornar valores únicos da coluna job_trybes

--- src/insights.py
+++ src/insights.py
@@ -1,3 +1,6 @@
+from src import jobs
+
+
 def get_unique_job_types(path):
     """Checks all different job types and returns a list of them
 
@@ -13,7 +16,10 @@ def get_unique_job_types(path):
     list
         List of unique job types
     """
-    return []
+    data = jobs.read(path)
+    result = [value["job_type"] for value in data]
+
+    return result
 
 
 def filter_by_job_type(jobs, job_type):
