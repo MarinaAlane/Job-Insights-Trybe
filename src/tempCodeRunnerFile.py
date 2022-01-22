1854d59fc546c2e3b6f60f8bfe58e4fcae75f05f
josanjohnata <josanjohnata@gmail.com>
2022-01-21 22:48:20 -0300

    Adiciona requisito 5

--- src/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-data_job = read(path)
-    salary = set()
-    for job in data_job:
-        if job["max_salary"].isnumeric():
-            salary.add(int(job["max_salary"]))
-
-    return max(salary)
