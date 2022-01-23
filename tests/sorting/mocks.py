bf51e9affbaf24b61fee74dbf166d28d8dc97253
IsaacMagno <isaacmagno98@gmail.com>
2022-01-23 14:10:33 -0300

    desafio10 vai agora em

--- tests/sorting/mocks.py
+++ tests/sorting/mocks.py
@@ -2,19 +2,25 @@ from datetime import date
 
 sort_date = [
     {"min_salary": 10, "max_salary": 1000, "date_posted": "2021-01-24"},
-    {"min_salary": 1000, "max_salary": 10000, "date_posted": "2020-01-24"},
+    {"min_salary": 1000, "max_salary": 10000, "date_posted": "2020-03-02"},
+    {"min_salary": 100, "max_salary": 200, "date_posted": "2019-04-14"},
+    {"min_salary": -50, "max_salary": 25, "date_posted": "2019-03-14"},
     {"min_salary": '', "max_salary": '', "date_posted": ''},
 ]
 
 sort_max_salary = [
-    {"min_salary": 1000, "max_salary": 10000, "date_posted": "2020-01-24"},
+    {"min_salary": 1000, "max_salary": 10000, "date_posted": "2020-03-02"},
     {"min_salary": 10, "max_salary": 1000, "date_posted": "2021-01-24"},
+    {"min_salary": 100, "max_salary": 200, "date_posted": "2019-04-14"},
+    {"min_salary": -50, "max_salary": 25, "date_posted": "2019-03-14"},
     {"min_salary": '', "max_salary": '', "date_posted": ''},
 ]
 
 sort_min_salary = [
-    {"min_salary": 1000, "max_salary": 10000, "date_posted": "2020-01-24"},
+    {"min_salary": -50, "max_salary": 25, "date_posted": "2019-03-14"},
     {"min_salary": 10, "max_salary": 1000, "date_posted": "2021-01-24"},
+    {"min_salary": 100, "max_salary": 200, "date_posted": "2019-04-14"},
+    {"min_salary": 1000, "max_salary": 10000, "date_posted": "2020-03-02"},
     {"min_salary": '', "max_salary": '', "date_posted": ''},
 ]
 
--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,4 @@
 from src.sorting import sort_by
-import pytest
 from mocks import (
   sort_date,
   sort_max_salary,
@@ -8,7 +7,9 @@ from mocks import (
 
 jobs = [
     {"min_salary": 10, "max_salary": 1000, "date_posted": "2021-01-24"},
-    {"min_salary": 1000, "max_salary": 10000, "date_posted": "2020-01-24"},
+    {"min_salary": 1000, "max_salary": 10000, "date_posted": "2020-03-02"},
+    {"min_salary": 100, "max_salary": 200, "date_posted": "2019-04-14"},
+    {"min_salary": -50, "max_salary": 25, "date_posted": "2019-03-14"},
     {"min_salary": '', "max_salary": '', "date_posted": ''},
 ]
 
@@ -22,9 +23,3 @@ def test_sort_by_criteria():
 
     sort_by(jobs, 'min_salary')
     assert jobs == sort_min_salary
-
-    invalid = ["last_salary", "first_salary"]
-    for inv in invalid:
-        with pytest.raises(
-          ValueError, match=f"invalid sorting criteria: {inv}"):
-            sort_by(jobs, inv)
