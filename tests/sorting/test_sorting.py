15cc6ecb55fc9fabce7b292ac49203ab4966d5bf
Marcelo <marsleite@gmail.com>
2022-01-23 16:41:53 -0300

    [feat] test sort() invalid criteria

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,29 @@
-# from src.sorting import sort_by
+import pytest
+from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    """
+    Test sort_by() with various criteria.
+    """
+    jobs = [
+        {
+            "max_salary": "10000",
+            "min_salary": "5000",
+            "date_posted": "2019-01-01"
+        },
+        {
+            "max_salary": "9600",
+            "min_salary": "4000",
+            "date_posted": "2019-01-02"
+        },
+        {
+            "max_salary": "7890",
+            "min_salary": "2000",
+            "date_posted": "2019-01-03"
+        },
+    ]
+
+    # invalid criteria
+    with pytest.raises(ValueError):
+        sort_by(jobs, "invalid")
