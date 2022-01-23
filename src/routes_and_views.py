a23f1a333fa0cf4235d4c4cc7759f69254c8bec6
josanjohnata <josanjohnata@gmail.com>
2022-01-23 19:49:23 -0300

    Corrige requisito 11

--- src/routes_and_views.py
+++ src/routes_and_views.py
@@ -15,7 +15,7 @@ from .more_insights import (
     slice_jobs,
     get_int_from_args,
     build_jobs_urls,
-    get_jobs,
+    get_job,
 )
 
 bp = Blueprint("client", __name__, template_folder="templates")
@@ -67,7 +67,7 @@ def list_jobs():
 @bp.route("/job/<index>")
 def job(index):
     jobs = read("src/jobs.csv")
-    job = get_jobs(jobs, index)
+    job = get_job(jobs, index)
 
     return render_template("job.jinja2", job=job)
 
