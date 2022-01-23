0de06573af76318a74071d900f73cac536451f4d
Marcelo <marsleite@gmail.com>
2022-01-23 17:39:26 -0300

    [feat] sort max-salary, min_salary and data_posted

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,29 +1,40 @@
 import pytest
 from src.sorting import sort_by
 
+jobs = [
+    {"min_salary": 1500, "max_salary": 3000, "date_posted": "2021-02-01"},
+    {"min_salary": 3680, "max_salary": 5000, "date_posted": "2022-01-01"},
+    {"min_salary": 2740, "max_salary": 4000, "date_posted": "2019-03-03"},
+]
+
 
 def test_sort_by_criteria():
     """
     Test sort_by() with various criteria.
     """
-    jobs = [
-        {
-            "max_salary": "10000",
-            "min_salary": "5000",
-            "date_posted": "2019-01-01"
-        },
-        {
-            "max_salary": "9600",
-            "min_salary": "4000",
-            "date_posted": "2019-01-02"
-        },
-        {
-            "max_salary": "7890",
-            "min_salary": "2000",
-            "date_posted": "2019-01-03"
-        },
-    ]
-
-    # invalid criteria
     with pytest.raises(ValueError):
         sort_by(jobs, "invalid")
+
+    # Test sort_by() with max_salary
+    sort_by(jobs, "max_salary")
+    assert jobs == [
+        {"min_salary": 3680, "max_salary": 5000, "date_posted": "2022-01-01"},
+        {"min_salary": 2740, "max_salary": 4000, "date_posted": "2019-03-03"},
+        {"min_salary": 1500, "max_salary": 3000, "date_posted": "2021-02-01"},
+    ]
+
+    # Test sort_by() with min_salary
+    sort_by(jobs, "min_salary")
+    assert jobs == [
+        {"min_salary": 1500, "max_salary": 3000, "date_posted": "2021-02-01"},
+        {"min_salary": 2740, "max_salary": 4000, "date_posted": "2019-03-03"},
+        {"min_salary": 3680, "max_salary": 5000, "date_posted": "2022-01-01"},
+    ]
+
+    # Test sort_by() with date_posted
+    sort_by(jobs, "date_posted")
+    assert jobs == [
+        {"min_salary": 3680, "max_salary": 5000, "date_posted": "2022-01-01"},
+        {"min_salary": 1500, "max_salary": 3000, "date_posted": "2021-02-01"},
+        {"min_salary": 2740, "max_salary": 4000, "date_posted": "2019-03-03"},
+    ]
