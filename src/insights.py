80356e6a66f6fa3d08ddb68aecb97936daca0dc5
gabiazevedo <gabiazevedoms@gmail.com>
2022-01-23 15:14:57 -0300

    feat: Tenta passar requisito 8 - Não passa localmente

--- src/insights.py
+++ src/insights.py
@@ -37,21 +37,14 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    filtered_industries = []
+    try:
+        for industry in jobs:
+            if (industry["industry"] == industry):
+                filtered_industries.append(industry)
+    except ValueError:
+        print("The value of argument is invalid!!!")
+    return filtered_industries
 
 
 def get_max_salary(path):
