5b122fdd68d04b53fe66b3ff6bae22c507f34144
Marcelo <marsleite@gmail.com>
2022-01-23 11:52:38 -0300

    [feat] job types list returned

--- src/insights.py
+++ src/insights.py
@@ -1,3 +1,6 @@
+from src import jobs
+
+
 def get_unique_job_types(path):
     """Checks all different job types and returns a list of them
 
@@ -13,7 +16,11 @@ def get_unique_job_types(path):
     list
         List of unique job types
     """
-    return []
+    jobs_total = jobs.read(path)
+    job_types = []
+    for job in jobs_total:
+        job_types.append(job["job_type"])
+    return list(set(job_types))
 
 
 def filter_by_job_type(jobs, job_type):
