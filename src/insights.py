80d6b81d73a36dda0b62fe4a3f42b875c8ab0ec6
Marcelo <marsleite@gmail.com>
2022-01-23 13:43:18 -0300

    filter industry by job

--- src/insights.py
+++ src/insights.py
@@ -27,21 +27,8 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    ind_by_job = filter(lambda job: job["industry"] == industry, jobs)
+    return list(ind_by_job)
 
 
 def get_max_salary(path):
