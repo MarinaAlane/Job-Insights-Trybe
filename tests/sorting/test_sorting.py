afc647e923f6f2df846e97cfcc3182643ed80a80
Erick-Marinho <erickgaves@gmail.com>
2022-01-23 01:42:39 -0300

    Criando função para teste de ordenação

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,20 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    jobs = [
+        {"min_salary": ""},
+        {"min_salary": 10},
+        {"min_salary": 30},
+        {"min_salary": 20},
+        {"min_salary": 5},
+    ]
+
+    sort_by(jobs, "min_salary")
+    assert jobs == [
+        {"min_salary": ""},
+        {"min_salary": 5},
+        {"min_salary": 10},
+        {"min_salary": 20},
+        {"min_salary": 30},
+    ]
