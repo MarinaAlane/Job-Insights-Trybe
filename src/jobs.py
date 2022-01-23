ca51dc030905a0b6339c1abd17f3a34051c0a3f3
regoraphael <raphael.mlrego@gmail.com>
2022-01-22 21:21:13 -0300

    req 01 -  created method read

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,12 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    with open(path) as file:
+        content = csv.DictReader(file)
+        response = []
+        for item in content:
+            response.append(item)
+        return response
