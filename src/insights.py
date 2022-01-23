de4b36de9f3c83f79cb18170d3537453ed39a3af
josanjohnata <josanjohnata@gmail.com>
2022-01-23 18:18:21 -0300

    Adiciona requisito 6

--- src/insights.py
+++ src/insights.py
@@ -11,21 +11,8 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+    filter_job = list(filter(lambda job: job["job_type"] == job_type, jobs))
+    return filter_job
 
 
 def get_unique_industries(path):
