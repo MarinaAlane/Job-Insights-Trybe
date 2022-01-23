70bd761ad93e15b4578c3b1834b1f28390fceb95
Victor Faria <vitilevi@hotmail.com>
2022-01-23 19:30:44 -0300

    Remove lista nao utilizada

--- src/insights.py
+++ src/insights.py
@@ -219,13 +219,3 @@ def filter_by_salary_range(jobs, salary):
         except Exception:
             continue
     return list
-
-
-jobss = [
-        {"max_salary": 0, "min_salary": 10},
-        {"max_salary": 10, "min_salary": 100},
-        {"max_salary": 10000, "min_salary": 200},
-        {"max_salary": 15000, "min_salary": 0},
-        {"max_salary": 1500, "min_salary": 0},
-        {"max_salary": -1, "min_salary": 10},
-    ]
