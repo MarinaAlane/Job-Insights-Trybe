95da0789559b98510e978eb1ba8a33fe24439246
gabiazevedo <gabiazevedoms@gmail.com>
2022-01-22 21:06:55 -0300

    Sobe desenvolvimento parcial requisito 2

--- src/insights.py
+++ src/insights.py
@@ -1,19 +1,12 @@
+from src.jobs import read
+
+
 def get_unique_job_types(path):
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    return []
+    jobs = read(path)
+    types = []
+    for job in jobs:
+        types.append(job["job_type"])
+    return types
 
 
 def filter_by_job_type(jobs, job_type):
