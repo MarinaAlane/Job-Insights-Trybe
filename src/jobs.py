3f9fed60e0081fc7f0b631b8b6de456802e768ff
Renan <renan.vamo@gmail.com>
2022-01-23 10:55:46 -0300

    feat: cria funcao que resgata os valores do csv e retorna em uma lista

--- src/jobs.py
+++ src/jobs.py
@@ -1,7 +1,9 @@
 from functools import lru_cache
+import pandas as pd
+import json
 
 
-@lru_cache
+# @lru_cache
 def read(path):
     """Reads a file from a given path and returns its contents
 
@@ -15,4 +17,9 @@ def read(path):
     list
         List of rows as dicts
     """
-    return []
+    file = pd.read_csv(path, dtype=str, index_col=False)
+    chaves = file.columns
+    result = []
+    for _, row in file.iterrows():
+        result.append({chave: row[chave] for chave in chaves})
+    return result
