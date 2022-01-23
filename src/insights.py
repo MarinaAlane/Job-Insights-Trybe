219552f3e95ccec53db7eb6ee9ce601fc968f9c8
felipebuso <felipe_buso@hotmail.com>
2022-01-23 17:53:14 -0300

    feat: requisito 8

--- src/insights.py
+++ src/insights.py
@@ -59,29 +59,20 @@ def get_min_salary(path):
 
 
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
 
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
+    if "max_salary" not in job or "min_salary" not in job:
+        raise ValueError("Chave max_salary ou min_salary ausentes")
 
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
+    if type(job["max_salary"]) != int or type(job["min_salary"]) != int:
+        raise ValueError("Chave max_salary ou min_salary não é um inteiro")
 
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
+    if type(salary) != int:
+        raise ValueError("Salary não é um inteiro")
+
+    if job["max_salary"] > job["min_salary"]:
+        return job["max_salary"] >= salary >= job["min_salary"]
+    else:
+        raise ValueError("max_salary deve ser maior que min_salary")
 
 
 def filter_by_salary_range(jobs, salary):
