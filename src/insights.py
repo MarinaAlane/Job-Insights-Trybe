a7b81e9ac94d8a4d215d1154bff3de750bc1faf5
Renan <renan.vamo@gmail.com>
2022-01-23 12:16:52 -0300

    feat: resgata o valor unico de industries do arquivo

--- src/insights.py
+++ src/insights.py
@@ -53,7 +53,11 @@ def get_unique_industries(path):
     list
         List of unique industries
     """
-    return []
+    jobs = read(path)
+    industries = {job["industry"] for job in jobs}
+    if "" in industries:
+        industries.remove("")
+    return industries
 
 
 def filter_by_industry(jobs, industry):
