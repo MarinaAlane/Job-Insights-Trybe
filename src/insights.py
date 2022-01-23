2b8e3273a938d621bd6d6cf7efe6a2827bb9ed61
diogotrescastro <diogotrescastro@gmail.com>
2022-01-23 13:45:03 -0300

    requisito 8

--- src/insights.py
+++ src/insights.py
@@ -45,31 +45,28 @@ def get_min_salary(path):
             job_salary.add(int(job["min_salary"]))
     return min(job_salary)
 
+# Consulta na documentação e materials para compreensão de lançamento de erros
+#  e também sobre o  'not in':
+# https://docs.python.org/pt-br/3.8/tutorial/errors.html
+# https://www.w3schools.com/python/python_operators.asp
+# http://excript.com/python/operadores-in-not-in-python.html
+
 
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
 
-    Parameterss
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
+    if "max_salary" not in job or "min_salary" not in job:
+        raise ValueError
+    if (
+        type(salary) != int
+        or type(job["min_salary"]) != int
+        or type(job["max_salary"]) != int
+    ):
+        raise ValueError
 
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
+    if job["min_salary"] > job["max_salary"]:
+        raise ValueError()
 
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
+    return job["min_salary"] <= salary <= job["max_salary"]
 
 
 def filter_by_salary_range(jobs, salary):
