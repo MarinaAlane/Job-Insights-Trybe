8574bd396f104cc668275aa5ac02fafb4b02fb97
Matheus Antonio <matheusantonio232@gmail.com>
2022-01-23 23:42:06 -0300

    fix

--- src/insights.py
+++ src/insights.py
@@ -5,8 +5,8 @@ def get_unique_job_types(path):
     file = jobs.read(path)
     job_types_list = set()
     for row in file:
-        if row["date_posted"]:
-            job_types_list.add(row["date_posted"])
+        if row["job_type"]:
+            job_types_list.add(row["job_type"])
     return job_types_list
 
 
