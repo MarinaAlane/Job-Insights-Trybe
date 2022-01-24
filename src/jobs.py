34e657a327a9978ea0ea61f0366dc041498ab285
Samuel Silva Melo <samuel.silva.melo@hotmail.com>
2022-01-24 19:39:51 -0300

    FEAT: Requirement 1

--- src/jobs.py
+++ src/jobs.py
@@ -1,4 +1,5 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
@@ -15,4 +16,13 @@ def read(path):
     list
         List of rows as dicts
     """
-    return []
+
+    result = []
+
+    with open(path) as file:
+        file_reader = csv.DictReader(file, delimiter=",", quotechar='"')
+
+        for job in file_reader:
+            result.append(job)
+
+    return result
