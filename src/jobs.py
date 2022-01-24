9e79877f90afa205eec377fac882bb90fb0d182a
Thiago Gomes <thiagog.macedom@gmail.com>
2022-01-24 16:35:04 -0300

    add: task 1 done

--- src/jobs.py
+++ src/jobs.py
@@ -1,8 +1,19 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
+    try:
+        with open(path) as file:
+            jobs = csv.DictReader(file)
+            jobs_list = []
+            for job in jobs:
+                jobs_list.append(job)
+            return jobs_list
+    except FileNotFoundError:
+        print('Por favor insira um arquivo válido')
+
     """Reads a file from a given path and returns its contents
 
     Parameters
@@ -15,4 +26,5 @@ def read(path):
     list
         List of rows as dicts
     """
-    return []
+
+# print(read('./jobs.csv'))
