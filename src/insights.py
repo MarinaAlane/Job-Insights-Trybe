baaad47b166de7dd0902d49f07e16cce6a617dff
marlon307 <marlon-ramosb@hotmail.com>
2022-01-24 08:48:37 -0300

    Rq 8

--- src/insights.py
+++ src/insights.py
@@ -47,33 +47,19 @@ def get_min_salary(path):
     return value
 
 
-# print(get_min_salary("src/jobs.csv"))
-
-
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
+    if not isinstance(salary, int):
+        raise ValueError
+    if "min_salary" not in job or "max_salary" not in job:
+        raise ValueError
+    if (
+        type(job["min_salary"]) is not int
+        or type(job["max_salary"]) is not int
+    ):
+        raise ValueError
+    if int(job["min_salary"]) > int(job["max_salary"]):
+        raise ValueError
+    return job["max_salary"] >= salary >= job["min_salary"]
 
 
 def filter_by_salary_range(jobs, salary):
