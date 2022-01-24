5e9dd802c9d5be608bf494780909fc3ccde6a304
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2022-01-24 11:04:29 -0300

    feat: test_sort_by_criteria

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,15 @@
-# from src.sorting import sort_by
+import pytest
+from src.sorting import sort_by
 
 
-def test_sort_by_criteria():
-    pass
+@pytest.fixture
+def jobs():
+    return [
+        {"min_salary": 10, "max_salary": 100, "date_posted": "2020-05-08"},
+        {"min_salary": 1, "max_salary": 60, "date_posted": "2020-09-18"},
+        {"min_salary": 32, "max_salary": -1, "date_posted": "2020-05-04"},
+    ]
+
+
+def test_sort_by_criteria(jobs):
+    assert sort_by(jobs, "min_salary") is None
