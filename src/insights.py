e5c101f1aaadf4bf523f9118a9bd2cb068de00ce
Hugosomers <hugosomersla@gmail.com>
2022-01-24 09:09:46 -0300

    Rquisito 3

--- src/insights.py
+++ src/insights.py
@@ -31,21 +31,12 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    data = read(path)
+    industries = []
+    for index in data:
+        if index["industry"] != "":
+            industries.append(index["industry"])
+    return set(industries)
 
 
 def filter_by_industry(jobs, industry):
