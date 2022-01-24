bf4bb1fc1d3924856d93085dbabbc0711c973736
Matheus Antonio <matheusantonio232@gmail.com>
2022-01-23 23:46:25 -0300

    fix: testes

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -8,21 +8,21 @@ jobs = [
 ]
 
 mock_min_salary = [
-    {"min_salary": 1100, "max_salary": 6000, "date_posted": "2022-01-15"},
-    {"min_salary": 5000, "max_salary": 11000, "date_posted": "2022-01-20"},
-    {"min_salary": 10000, "max_salary": 25000, "date_posted": "2022-01-10"},
+    {"min_salary": 1100, "max_salary": 25000, "date_posted": "2022-01-20"},
+    {"min_salary": 5000, "max_salary": 11000, "date_posted": "2022-01-12"},
+    {"min_salary": 10000, "max_salary": 6000, "date_posted": "2022-01-10"},
 ]
 
 mock_max_salary = [
-    {"min_salary": 10000, "max_salary": 25000, "date_posted": "2022-01-10"},
-    {"min_salary": 5000, "max_salary": 11000, "date_posted": "2022-01-20"},
-    {"min_salary": 1100, "max_salary": 6000, "date_posted": "2022-01-15"},
+    {"min_salary": 1100, "max_salary": 25000, "date_posted": "2022-01-20"},
+    {"min_salary": 5000, "max_salary": 11000, "date_posted": "2022-01-12"},
+    {"min_salary": 10000, "max_salary": 6000, "date_posted": "2022-01-10"},
 ]
 
 mock_date_salary = [
-    {"min_salary": 5000, "max_salary": 11000, "date_posted": "2022-01-20"},
-    {"min_salary": 1100, "max_salary": 6000, "date_posted": "2022-01-15"},
-    {"min_salary": 10000, "max_salary": 25000, "date_posted": "2022-01-10"},
+    {"min_salary": 1100, "max_salary": 25000, "date_posted": "2022-01-20"},
+    {"min_salary": 5000, "max_salary": 11000, "date_posted": "2022-01-12"},
+    {"min_salary": 10000, "max_salary": 6000, "date_posted": "2022-01-10"},
 ]
 
 
