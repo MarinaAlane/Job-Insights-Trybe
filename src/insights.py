ef9b67d2c1de954a2704e0e3f053755dd1b350de
Bernardo Prado <bernardoprado22@yahoo.com.br>
2022-01-24 15:05:35 -0300

    req03

--- src/insights.py
+++ src/insights.py
@@ -12,21 +12,17 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
+    all_ind = read(path)
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    ind = set()
+    for item in all_ind:
+        industry = item['industry']
+        if industry != '':
+            ind.add(item['industry'])
+    print(ind)
+    return ind
+    # return set([ind["industry"] for ind in read(path) if ind != ''])
+    # tentando refatorar ainda
 
 
 def filter_by_industry(jobs, industry):
