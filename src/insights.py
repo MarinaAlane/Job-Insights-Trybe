d4a7a9ce0236dcc98b38dcb2b85ba573e5973039
leofuna <leo.funa@gmail.com>
2022-01-24 17:29:13 -0300

    Acerta funcao get_unique_industries

--- src/insights.py
+++ src/insights.py
@@ -12,21 +12,13 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
+    """Filters a list of jobs by job_type"""
+    jobs_filtered = []
+    for job in jobs:
+        if job["job_type"] == job_type:
+            jobs_filtered.append(job)
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+    return jobs_filtered
 
 
 def get_unique_industries(path):
@@ -34,7 +26,7 @@ def get_unique_industries(path):
     jobs_in_list = src.jobs.read(path)
     unique_industries_type = []
     for job in jobs_in_list:
-        if job["industry"] not in unique_industries_type:
+        if job["industry"] not in unique_industries_type and job["industry"] != "":
             unique_industries_type.append(job["industry"])
     return unique_industries_type
 
