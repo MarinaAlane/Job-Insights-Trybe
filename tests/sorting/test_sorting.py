8d0ce7def4ebc098612519ee4762bff8da523ef7
benlopes <rubenssouss@hotmail.com>
2022-01-24 10:50:41 -0300

    feat: test if passed wrong criteria sort_by() raises valueError

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,16 @@
-# from src.sorting import sort_by
+import pytest
+from src.sorting import sort_by
+from src.jobs import read
+
+criterias = [
+    "date_posted",
+    "max_salary",
+    "min_salary",
+]
 
 
 def test_sort_by_criteria():
-    pass
+    jobs_list = read("src/jobs.csv")
+
+    with pytest.raises(ValueError):
+        sort_by(jobs_list, "top_salary")
