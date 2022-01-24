e5a8d98bb5b9281fd6343c2cdae07037156b7406
Jessie <miss_sc_jessi@hotmail.com>
2022-01-24 16:58:33 -0300

    req 10.4 fix

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,4 +1,5 @@
 from src.sorting import sort_by
+import pytest
 
 jobs_list = [
     {'min_salary': 1594, 'max_salary': 2015, 'date_posted': '2020-04-19'},
@@ -29,3 +30,5 @@ def test_sort_by_criteria():
     assert sort_by(jobs_list, 'min_salary') == list_min_salary
     assert sort_by(jobs_list, 'max_salary') == list_max_salary
     assert sort_by(jobs_list, 'date_posted') == list_date_posted
+    with pytest.raises(ValueError):
+        sort_by(jobs_list, "incorrect")
