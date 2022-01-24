e2440bf2a3529625cbb4dc63dd668b2ae637f895
Wilquer82 <wilquer.fig@gmail.com>
2022-01-24 20:58:00 -0300

    req 1 lint

--- src/jobs.py
+++ src/jobs.py
@@ -1,11 +1,10 @@
 from functools import lru_cache
 import csv
-# hello
 
 
 @lru_cache
 def read(path):
-  with open(path) as file:
-  get_jobs = csv.DictReader(file, delimiter=",", quotechar='"')
-  content = list(get_jobs)
-return content
+    with open(path) as file:
+        get_jobs = csv.DictReader(file, delimiter=",", quotechar='"')
+        content = list(get_jobs)
+    return content
