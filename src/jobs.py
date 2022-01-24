e56e1eec7b5c59eee7d81121e12c489066e30ca2
GuiiCNunes <guiicnunes@gmail.com>
2022-01-24 20:50:45 -0300

    FEAT: Req 1

--- src/jobs.py
+++ src/jobs.py
@@ -1,4 +1,5 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
@@ -15,4 +16,10 @@ def read(path):
     list
         List of rows as dicts
     """
-    return []
+    with open(path) as file:
+        csv_reader = csv.DictReader(file, delimiter=",", quotechar='"')
+        response = []
+        for row in csv_reader:
+            response.append(row)
+
+    return response
