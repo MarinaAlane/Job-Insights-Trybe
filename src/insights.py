f4d912d89bd355f4de4ff8748a09178506a61248
Hugosomers <hugosomersla@gmail.com>
2022-01-24 09:46:39 -0300

    Requisito 7

--- src/insights.py
+++ src/insights.py
@@ -30,21 +30,11 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    filtered = []
+    for index in jobs:
+        if index["industry"] == industry:
+            filtered.append(index)
+    return filtered
 
 
 def get_max_salary(path):
