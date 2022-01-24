3b56f0ab7e7dadf04869e02ad5d8011f9cf3f365
benlopes <rubenssouss@hotmail.com>
2022-01-24 16:50:03 -0300

    fix: test all criteria sort_by() requirements

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -35,36 +35,37 @@ jobs_max_salary = [
     {"min_salary": '95665',
      "max_salary": '165443',
      "date_posted": '2020-05-02'},
+    {"min_salary": '90665',
+     "max_salary": '130000',
+     "date_posted": '2020-01-01'},
     {"min_salary": '10665',
      "max_salary": '80000',
      "date_posted": '2020-10-01'},
-    {"min_salary": '90665',
-     "max_salary": '130000',
-     "date_posted": '2020-01-01'},
 ]
 
 jobs_date_posted = [
-    {"min_salary": '90665',
-     "max_salary": '130000',
-     "date_posted": '2020-01-01'},
     {"min_salary": '10665',
      "max_salary": '80000',
      "date_posted": '2020-10-01'},
     {"min_salary": '95665',
      "max_salary": '165443',
      "date_posted": '2020-05-02'},
+    {"min_salary": '90665',
+     "max_salary": '130000',
+     "date_posted": '2020-01-01'},
 ]
 
 
 def test_sort_by_criteria():
+
+    sort_by(jobs, "min_salary")
+    assert jobs == jobs_min_salary
+
+    sort_by(jobs, "max_salary")
+    assert jobs == jobs_max_salary
+
+    sort_by(jobs, "date_posted")
+    assert jobs == jobs_date_posted
+
     with pytest.raises(ValueError):
         sort_by(jobs, "top_salary")
-
-    result_1 = sort_by(jobs, "min_salary")
-    assert result_1 == jobs_min_salary
-
-    result_2 = sort_by(jobs, "max_salary")
-    assert result_2 == jobs_max_salary
-
-    result_3 = sort_by(jobs, "date_posted")
-    assert result_3 == jobs_date_posted
