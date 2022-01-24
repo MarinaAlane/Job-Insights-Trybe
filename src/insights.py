56a51065e1d238492cb9d2665b59c72ef68b8806
heyset <c.inaciomatheus@gmail.com>
2022-01-24 18:55:46 -0300

    req-2: adiciona listagem unica de tipos de trampo

--- src/insights.py
+++ src/insights.py
@@ -1,3 +1,6 @@
+import src.jobs as jobs
+
+
 def get_unique_job_types(path):
     """Checks all different job types and returns a list of them
 
@@ -13,7 +16,9 @@ def get_unique_job_types(path):
     list
         List of unique job types
     """
-    return []
+    job_dict_list = jobs.read(path)
+    job_types = list(set([job["job_type"] for job in job_dict_list]))
+    return job_types
 
 
 def filter_by_job_type(jobs, job_type):
