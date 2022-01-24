169884c9e8a57065fc5f55fcfb4c7d3630617116
Thyago Pessoa <flash_thyago@hotmail.com>
2022-01-24 18:45:46 -0300

    Req 10.1

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,21 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+import pytest
 
 
-def test_sort_by_criteria():
+@pytest.fixture
+def jobs_for_sort_by_criteria():
+    return [
+        {"date_posted": "2020-05-17", "max_salary": 15, "min_salary": 10},
+        {"date_posted": "2020-12-02", "max_salary": 10000, "min_salary": 200},
+        {"date_posted": "2020-09-25", "max_salary": 150, "min_salary": 100},
+    ]
+
+
+def test_sort_by_criteria(jobs_for_sort_by_criteria):
     pass
+    sort_by(jobs_for_sort_by_criteria, "min_salary")
+    assert jobs_for_sort_by_criteria == [
+        {"date_posted": "2020-05-17", "max_salary": 15, "min_salary": 10},
+        {"date_posted": "2020-09-25", "max_salary": 150, "min_salary": 100},
+        {"date_posted": "2020-12-02", "max_salary": 10000, "min_salary": 200},
+    ]
