ffe1731ceb96e164ad16fd3c9661e93543aee593
Gustavo Miyagawa <miyagawasantos@hotmail.com>
2022-01-24 19:55:33 -0400

    Testa funcao sort_by max_salary. Req. 10.

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,20 @@
-# from src.sorting import sort_by
+import pytest
+from src.sorting import sort_by
 
 
-def test_sort_by_criteria():
-    pass
+@pytest.fixture
+def jobs():
+    return [
+        {"max_salary": 1000, "min_salary": 500, "date_posted": "1992-08-10"},
+        {"max_salary": 1500, "min_salary": 300, "date_posted": "1992-09-10"},
+        {"max_salary": 900, "min_salary": 50, "date_posted": "1992-01-10"},
+    ]
+
+
+def test_sort_by_criteria(jobs):
+    sort_by(jobs, "max_salary")
+    assert [
+        {"max_salary": 1500, "min_salary": 300, "date_posted": "1992-09-10"},
+        {"max_salary": 1000, "min_salary": 500, "date_posted": "1992-08-10"},
+        {"max_salary": 900, "min_salary": 50, "date_posted": "1992-01-10"},
+    ] == jobs
