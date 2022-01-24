1b912cdefcdf13b01deb9c50522bf2629c1eafc6
Gustavo Lemes <gustavolemes997@gmail.com>
2022-01-24 18:28:43 -0300

    Requisito 5

--- src/insights.py
+++ src/insights.py
@@ -71,20 +71,16 @@ def get_max_salary(path):
 
 
 def get_min_salary(path):
-    """Get the minimum salary of all jobs
+    jobs_list = read(path)
+    min_salary = 100000000
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
+    for job in jobs_list:
+        try:
+            if int(job["min_salary"]) < min_salary:
+                min_salary = int(job["min_salary"])
+        except ValueError:
+            pass
+    return min_salary
     pass
 
 
