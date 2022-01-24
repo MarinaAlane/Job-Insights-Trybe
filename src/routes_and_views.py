6ace006b3a84eae069d368bd82ed14a3d77207d7
Igor Marcos Riegel <igor.riegel@gmail.com>
2022-01-24 15:13:52 -0300

    Fix req 11-render view

--- src/routes_and_views.py
+++ src/routes_and_views.py
@@ -66,8 +66,7 @@ def list_jobs():
 
 @bp.route("/job/<index>")
 def job(index):
-    jobs = read(path="src/jobs.csv")
-    selected_job = get_job(jobs, index)
+    selected_job = get_job(read("src/jobs.csv"), index)
 
     return render_template("job.jinja2", job=selected_job)
 
