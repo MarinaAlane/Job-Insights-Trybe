f43f5c25016c6dd53eda6e452080af7f3d914185
Gustavo Lemes <gustavolemes997@gmail.com>
2022-01-24 18:16:26 -0300

    Requisito 3

--- src/insights.py
+++ src/insights.py
@@ -30,21 +30,13 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
+    jobs_list = read(path)
+    industries_list = []
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    for job in jobs_list:
+        if job["industry"] != '' and job["industry"] not in industries_list:
+            industries_list.append(job["industry"])
+    return industries_list
 
 
 def filter_by_industry(jobs, industry):
