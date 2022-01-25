5e02ed2526e0588099eee5112547d886103c89a7
leofuna <leo.funa@gmail.com>
2022-01-25 16:18:11 -0300

    Ajustes linter

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -17,4 +17,5 @@ def test_sort_by_criteria():
     assert data_to_sort == [
         {"min_salary": 800, "max_salary": 4000, "date_posted": "2022-01-20"},
         {"min_salary": 1000, "max_salary": 3000, "date_posted": "2022-01-23"},
-        {"min_salary": 1200, "max_salary": 3500, "date_posted": "2022-01-01"}]
+        {"min_salary": 1200, "max_salary": 3500, "date_posted": "2022-01-01"},
+    ]
