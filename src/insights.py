e826e3772e6209e7015fc1b35e6aee91fb52cbb9
hheloisa <hhackenhaar@gmail.com>
2022-01-25 00:23:13 -0300

    Primeira versao de resoluçao req. 7 - problemas avaliador local

--- src/insights.py
+++ src/insights.py
@@ -28,21 +28,12 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    #  o primeiro parametro que recebe já é uma lista
+    list_industries = []
+    for job in jobs:
+        if job["industry"] == industry:  # se for = ao do parametro
+            list_industries.append(job)
+    return list_industries
 
 
 def get_max_salary(path):
