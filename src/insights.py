6cb6a86db5e89867158e37514f681ddc3c39eab9
Breno Rodrigues <brenorm@gmail.com>
2022-01-24 22:07:53 -0300

    lint Fix

--- src/insights.py
+++ src/insights.py
@@ -29,6 +29,7 @@ def filter_by_job_type(jobs, job_type):
     """
     return [job for job in jobs if job["job_type"] == job_type]
 
+
 def get_unique_industries(path):
     jobs_list = read(path)
     industry_types = set()
