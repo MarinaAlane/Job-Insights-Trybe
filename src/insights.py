1470b484ccdbe0cef2a87e8a4c4b404f06d16044
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2022-01-25 18:09:22 -0300

    Req 8 - matches salary range

--- src/insights.py
+++ src/insights.py
@@ -58,29 +58,15 @@ def get_min_salary(path):
 
 
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
+    keys = ['max_salary', 'min_salary']
+    for key in keys:
+        if key not in job or type(job[key]) != int:
+            raise ValueError('min_salary or max_salary is not valid')
 
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
+    if job['min_salary'] > job['max_salary']:
+        raise ValueError("min_salary can't be greater than max_salary")
 
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
+    return salary >= job['min_salary'] and salary <= job['max_salary']
 
 
 def filter_by_salary_range(jobs, salary):
