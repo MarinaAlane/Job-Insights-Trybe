08b999b73816b5b84e8e8096f272367808f3ec75
Wilquer82 <wilquer.fig@gmail.com>
2022-01-25 09:57:22 -0300

    req 10 testes lint

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -15,21 +15,21 @@ def test_sort_by_criteria():
         {"min_salary": 1000, "max_salary": 4000, "date_posted": "2022-01-24"},
         {"min_salary": 3000, "max_salary": 3000, "date_posted": "2022-03-24"},
         {"min_salary": 2000, "max_salary": 2000, "date_posted": "2022-02-24"},
-    ]  # crescente max
+    ]
 
     sort_by(jobs, "min_salary")
     assert jobs == [
         {"min_salary": 1000, "max_salary": 4000, "date_posted": "2022-01-24"},
         {"min_salary": 2000, "max_salary": 2000, "date_posted": "2022-02-24"},
         {"min_salary": 3000, "max_salary": 3000, "date_posted": "2022-03-24"},
-    ]  # crescente min
+    ]
 
     sort_by(jobs, "date_posted")
     assert jobs == [
         {"min_salary": 3000, "max_salary": 3000, "date_posted": "2022-03-24"},
         {"min_salary": 2000, "max_salary": 2000, "date_posted": "2022-02-24"},
         {"min_salary": 1000, "max_salary": 4000, "date_posted": "2022-01-24"},
-    ]  #  decrescente date
+    ]
 
     with pytest.raises(ValueError):
         sort_by(jobs, "something")  # pega erros
