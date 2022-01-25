e792b00b02d3c8021d24f2dbfdcc4add1bdd1c76
Breno Rodrigues <brenorm@gmail.com>
2022-01-24 21:59:43 -0300

    Requisito 6: Retorna lista de empregos filtrada por tipo de emprego

--- src/insights.py
+++ src/insights.py
@@ -12,6 +12,7 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
+
     """Filters a list of jobs by job_type
 
     Parameters
@@ -26,8 +27,7 @@ def filter_by_job_type(jobs, job_type):
     list
         List of jobs with provided job_type
     """
-    return []
-
+    return [job for job in jobs if job["job_type"] == job_type]
 
 def get_unique_industries(path):
     jobs_list = read(path)
