ccf6b25b629badbb3152f0b52bb4d0c4d16f4620
Daniel Leon <daniel.radialista@gmail.com>
2022-01-24 22:18:27 -0300

    req. 10 pronto

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,19 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    jobs_values = [
+        {"min_salary": 100},
+        {"min_salary": 700},
+        {"min_salary": 250},
+        {"min_salary": 800},
+        {"min_salary": 340},
+    ]
+    sort_by(jobs_values, "min_salary")
+    assert jobs_values == [
+        {"min_salary": 100},
+        {"min_salary": 250},
+        {"min_salary": 340},
+        {"min_salary": 700},
+        {"min_salary": 800},
+    ]
