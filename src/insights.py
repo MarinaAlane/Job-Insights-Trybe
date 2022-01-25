d4d19e0ed9eb18b92d8be83cc1fc891495220609
lima08 <joaopaulo.gomeslima8@gmail.com>
2022-01-25 17:58:57 -0300

    Function filter_by_industry done.

--- src/insights.py
+++ src/insights.py
@@ -30,21 +30,12 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
+    all_jobs_list = read(path)
+    industry_values = set()
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    for job in all_jobs_list:
+        industry_values.add(job['industry'])
+    return industry_values
 
 
 def filter_by_industry(jobs, industry):
