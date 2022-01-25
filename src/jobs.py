2fd7084ee53a4486c7d02f59fee87c46cc6c18fd
juliana-happatsch <jhappatsch@gmail.com>
2022-01-25 20:05:38 -0300

    req 1

--- src/jobs.py
+++ src/jobs.py
@@ -1,4 +1,5 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
@@ -15,4 +16,12 @@ def read(path):
     list
         List of rows as dicts
     """
-    return []
+    result = []
+
+    with open(path) as file:
+        file_reader = csv.DictReader(file, delimiter=",", quotechar='"')
+
+        for thing in file_reader:
+            result.append(thing)
+
+    return result
