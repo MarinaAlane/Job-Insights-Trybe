3bb48c58cf700f36239abf9021250eb2e147afdd
Laura Gusmao <laurargus@gmail.com>
2022-01-25 14:42:37 -0300

    Requisito 8

--- src/insights.py
+++ src/insights.py
@@ -54,30 +54,23 @@ def get_min_salary(path):
     return min(job_salaries)
 
 
+# Ref: https://www.w3schools.com/python/python_operators.asp (not in, is not)
+# https://www.w3schools.com/python/python_try_except.asp (raise)
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
+    if "min_salary" not in job or "max_salary" not in job:
+        raise ValueError()
 
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
+    if (
+        type(job["min_salary"]) is not int or
+        type(job["max_salary"]) is not int or
+        type(salary) is not int
+    ):
+        raise ValueError()
 
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
+    if job["min_salary"] > job["max_salary"]:
+        raise ValueError()
 
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
+    return job["min_salary"] <= salary <= job["max_salary"]
 
 
 def filter_by_salary_range(jobs, salary):
