a8794b4eb617a9eef253aaa2cedc14e2b7ed198e
marlon307 <marlon-ramosb@hotmail.com>
2022-01-25 08:50:09 -0300

    Rq 10

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,24 @@
 # from src.sorting import sort_by
+from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
     pass
+    jobs = [
+        {'max_salary': 10, 'min_salary': 5, 'date_posted': '2021-01-01'},
+        {'max_salary': 20, 'min_salary': 1, 'date_posted': '2021-01-02'},
+        {'max_salary': 30, 'min_salary': 2, 'date_posted': '2021-01-03'},
+    ]
+    sort_by(jobs, 'max_salary')
+    assert jobs == [
+        {'max_salary': 30, 'min_salary': 2, 'date_posted': '2021-01-03'},
+        {'max_salary': 20, 'min_salary': 1, 'date_posted': '2021-01-02'},
+        {'max_salary': 10, 'min_salary': 5, 'date_posted': '2021-01-01'},
+    ]
+
+    sort_by(jobs, 'min_salary')
+    assert jobs == [
+        {'max_salary': 20, 'min_salary': 1, 'date_posted': '2021-01-02'},
+        {'max_salary': 30, 'min_salary': 2, 'date_posted': '2021-01-03'},
+        {'max_salary': 10, 'min_salary': 5, 'date_posted': '2021-01-01'},
+    ]
