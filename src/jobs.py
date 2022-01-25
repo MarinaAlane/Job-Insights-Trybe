efa4cb1c921f83f850e3be1febadb84014a06ca5
lucasSCsantos <luc.cristovam10@gmail.com>
2022-01-25 13:42:19 -0300

    req 1 correct

--- src/jobs.py
+++ src/jobs.py
@@ -1,14 +1,15 @@
 from functools import lru_cache
 import csv
 
+
 @lru_cache
 def read(path):
-    
+
     arr = []
 
     with open(path) as file:
         jobs = csv.DictReader(file, delimiter=",", quotechar='"')
         for job in jobs:
             arr.append(job)
-   
+
     return arr
