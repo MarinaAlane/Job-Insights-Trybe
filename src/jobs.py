92ff1c8ecc3e58e3065f95752a3daef7572a6dc8
hheloisa <hhackenhaar@gmail.com>
2022-01-24 22:43:12 -0300

    Corrige identaçao, requisito 1

--- src/jobs.py
+++ src/jobs.py
@@ -1,12 +1,13 @@
 from functools import lru_cache
-import csv #importa csv para poder ler os dados
+import csv
+
 
 @lru_cache
 def read(path):
     list = []
     with open(path) as job_file:
         job_list = csv.DictReader(job_file)
-    for job in job_list:
-        list.append(job)
-   
+        for job in job_list:
+            list.append(job)
+
     return list
