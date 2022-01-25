9a52a286dbdf217ce79b2b17af868ca45e6a410b
Wilquer82 <wilquer.fig@gmail.com>
2022-01-25 09:46:41 -0300

    req 10 testes

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,35 @@
-# from src.sorting import sort_by
+import pytest
+from src.sorting import sort_by
+
+jobs = [
+    {"min_salary": 1000, "max_salary": 4000, "date_posted": "2022-01-24"},
+    {"min_salary": 3000, "max_salary": 3000, "date_posted": "2022-03-24"},
+    {"min_salary": 2000, "max_salary": 2000, "date_posted": "2022-02-24"},
+]  # Mock dos Dados para os testes
 
 
 def test_sort_by_criteria():
-    pass
+
+    sort_by(jobs, "max_salary")
+    assert jobs == [
+        {"min_salary": 1000, "max_salary": 4000, "date_posted": "2022-01-24"},
+        {"min_salary": 3000, "max_salary": 3000, "date_posted": "2022-03-24"},
+        {"min_salary": 2000, "max_salary": 2000, "date_posted": "2022-02-24"},
+    ]  # crescente max
+
+    sort_by(jobs, "min_salary")
+    assert jobs == [
+        {"min_salary": 1000, "max_salary": 4000, "date_posted": "2022-01-24"},
+        {"min_salary": 2000, "max_salary": 2000, "date_posted": "2022-02-24"},
+        {"min_salary": 3000, "max_salary": 3000, "date_posted": "2022-03-24"},
+    ]  # crescente min
+
+    sort_by(jobs, "date_posted")
+    assert jobs == [
+        {"min_salary": 3000, "max_salary": 3000, "date_posted": "2022-03-24"},
+        {"min_salary": 2000, "max_salary": 2000, "date_posted": "2022-02-24"},
+        {"min_salary": 1000, "max_salary": 4000, "date_posted": "2022-01-24"},
+    ]  #  decrescente date
+
+    with pytest.raises(ValueError):
+        sort_by(jobs, "something")  # pega erros
