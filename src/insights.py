7bfa6b0b3bba1876bb0f5667dc415fc1bb49c2c5
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2022-01-25 13:37:55 -0300

    Req 3 - get unique industries

--- src/insights.py
+++ src/insights.py
@@ -28,21 +28,13 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    csv_array = read(path)
+    industrias = set()
+    for item in csv_array:
+        industria = item["industry"]
+        if (industria != ''):
+            industrias.add(industria)
+    return industrias
 
 
 def filter_by_industry(jobs, industry):
