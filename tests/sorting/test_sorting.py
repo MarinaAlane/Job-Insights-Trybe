67dc435b7ac397dcea97a3b95ea4573ee6db466d
Tulio Sploradori <tuliospl@gmail.com>
2022-01-25 13:11:41 -0300

    Req 10

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -3,32 +3,32 @@ import pytest
 
 
 jobs = [
-        {'min_salary': 6, 'max_salary': 35, 'date_posted': '2010-10-10'},
-        {'min_salary': 10, 'max_salary': 30, 'date_posted': '2010-10-10'},
-        {'min_salary': 55, 'max_salary': 120, 'date_posted': '2010-10-10'},
+        {'min_salary': 6, 'max_salary': 35, 'date_posted': '2010-10-12'},
+        {'min_salary': 10, 'max_salary': 30, 'date_posted': '2010-10-16'},
+        {'min_salary': 55, 'max_salary': 120, 'date_posted': '2010-10-20'},
 ]
 
 
 def test_sort_by_criteria():
     sort_by(jobs, 'min_salary')
     assert jobs == [
-        {'min_salary': 6, 'max_salary': 35, 'date_posted': '2010-10-10'},
-        {'min_salary': 10, 'max_salary': 30, 'date_posted': '2010-10-10'},
-        {'min_salary': 55, 'max_salary': 120, 'date_posted': '2010-10-10'},
+        {'min_salary': 6, 'max_salary': 35, 'date_posted': '2010-10-12'},
+        {'min_salary': 10, 'max_salary': 30, 'date_posted': '2010-10-16'},
+        {'min_salary': 55, 'max_salary': 120, 'date_posted': '2010-10-20'},
     ]
 
     sort_by(jobs, 'max_salary')
     assert jobs == [
-        {'min_salary': 6, 'max_salary': 35, 'date_posted': '2010-10-10'},
-        {'min_salary': 10, 'max_salary': 30, 'date_posted': '2010-10-10'},
-        {'min_salary': 55, 'max_salary': 120, 'date_posted': '2010-10-10'},
+        {'min_salary': 55, 'max_salary': 120, 'date_posted': '2010-10-20'},
+        {'min_salary': 6, 'max_salary': 35, 'date_posted': '2010-10-12'},
+        {'min_salary': 10, 'max_salary': 30, 'date_posted': '2010-10-16'},
     ]
 
     sort_by(jobs, 'date_posted')
     assert jobs == [
-        {'min_salary': 6, 'max_salary': 35, 'date_posted': '2010-10-10'},
-        {'min_salary': 10, 'max_salary': 30, 'date_posted': '2010-10-10'},
-        {'min_salary': 55, 'max_salary': 120, 'date_posted': '2010-10-10'},
+        {'min_salary': 55, 'max_salary': 120, 'date_posted': '2010-10-20'},
+        {'min_salary': 10, 'max_salary': 30, 'date_posted': '2010-10-16'},
+        {'min_salary': 6, 'max_salary': 35, 'date_posted': '2010-10-12'},
     ]
 
     with pytest.raises(ValueError):
