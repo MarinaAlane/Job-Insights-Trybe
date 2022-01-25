6aa5bbafdad7b14a112e991c6a4235f9d87d3c95
IsleneGomes <islenegomes@gmail.com>
2022-01-25 12:40:37 -0300

    Implementa filter

--- src/insights.py
+++ src/insights.py
@@ -13,21 +13,8 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+    by_job_type = list(filter(lambda job: job["job_type"] == job_type, jobs))
+    return by_job_type
 
 
 def get_unique_industries(path):
@@ -42,21 +29,8 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    by_industry = filter(lambda job: job["industry"] == industry, jobs)
+    return list(by_industry)
 
 
 def get_max_salary(path):
