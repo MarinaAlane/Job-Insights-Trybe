8cd5cde6907e8df43aa06108960fb7ef664b6986
leofuna <leo.funa@gmail.com>
2022-01-25 16:22:09 -0300

    exercicio 10 testes com max salary e date posted

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -19,3 +19,17 @@ def test_sort_by_criteria():
         {"min_salary": 1000, "max_salary": 3000, "date_posted": "2022-01-23"},
         {"min_salary": 1200, "max_salary": 3500, "date_posted": "2022-01-01"},
     ]
+    data_to_sort = mocked_jobs
+    sort_by(data_to_sort, "max_salary")
+    assert data_to_sort == [
+        {"min_salary": 800, "max_salary": 4000, "date_posted": "2022-01-20"},
+        {"min_salary": 1200, "max_salary": 3500, "date_posted": "2022-01-01"},
+        {"min_salary": 1000, "max_salary": 3000, "date_posted": "2022-01-23"},
+    ]
+    data_to_sort = mocked_jobs
+    sort_by(data_to_sort, "date_posted")
+    assert data_to_sort == [
+        {"min_salary": 1000, "max_salary": 3000, "date_posted": "2022-01-23"},
+        {"min_salary": 800, "max_salary": 4000, "date_posted": "2022-01-20"},
+        {"min_salary": 1200, "max_salary": 3500, "date_posted": "2022-01-01"},
+    ]
