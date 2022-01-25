e574dffce73a2dfaa1118c237f2e7c6b4497f400
Peryclesreis <perycles_reis@hotmail.com>
2022-01-24 23:03:30 -0300

    commit inicial req 1

--- src/jobs.py
+++ src/jobs.py
@@ -1,4 +1,5 @@
-from functools import lru_cache
+from functools import lru_cache;
+import csv;
 
 
 @lru_cache
@@ -15,4 +16,6 @@ def read(path):
     list
         List of rows as dicts
     """
-    return []
+    with open(path) as file:
+        read_file = csv.DictReader(file)
+    return [dict(row) for row in read_file]
