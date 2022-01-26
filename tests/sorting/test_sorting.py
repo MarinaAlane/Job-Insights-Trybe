7bc9897da8c5c030d8a08553993527cb95cb7574
Breno Rodrigues <brenorm@gmail.com>
2022-01-25 23:29:26 -0300

    Lint Fix

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -3,5 +3,8 @@ from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    with pytest.raises(ValueError, match="invalid sorting criteria: invalid_criteria"):
+    with pytest.raises(
+        ValueError,
+        match="invalid sorting criteria: invalid_criteria"
+    ):
         sort_by(jobs=[], criteria="invalid_criteria")
