76825fe05da2cc1b80e9df4e5a20a01b5c1603b1
Guilherme Pereira <guilhermepdo1508@gmail.com>
2022-01-25 21:18:26 -0300

    feat: feito requisito 01

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,14 @@
 from functools import lru_cache
+from csv import DictReader
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
+    file_content_list = []
 
-    Parameters
-    ----------
-    path : str
-        Full path to file
+    with open(path) as file:
+        file_content = DictReader(file, delimiter=",", quotechar='"')
+        for row in file_content:
+            file_content_list.append(row)
 
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    return file_content_list
