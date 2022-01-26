f6d0a51c62745639dd9580028127e2438587e302
lima08 <joaopaulo.gomeslima8@gmail.com>
2022-01-26 12:29:26 -0300

    Add requirement 08 - matches_salary_range

--- src/insights.py
+++ src/insights.py
@@ -6,13 +6,13 @@ def get_unique_job_types(path):  # ok
     unique_job = set()
 
     for job in all_jobs_list:
-        if job['job_type'] is not None:
-            unique_job.add(job['job_type'])
+        if job["job_type"] is not None:
+            unique_job.add(job["job_type"])
     return unique_job
 
 
 def filter_by_job_type(jobs, job_type):  # ok
-    job_list = [job for job in jobs if job['job_type'] == job_type]
+    job_list = [job for job in jobs if job["job_type"] == job_type]
     return job_list
 
 
@@ -21,13 +21,13 @@ def get_unique_industries(path):  # ok
     industry_values = set()
 
     for job in all_jobs_list:
-        if job['industry'] != '':
-            industry_values.add(job['industry'])
+        if job["industry"] != "":
+            industry_values.add(job["industry"])
     return industry_values
 
 
-def filter_by_industry(jobs, industry):
-    job_list = [job for job in jobs if job['industry'] == industry]
+def filter_by_industry(jobs, industry):  # ok
+    job_list = [job for job in jobs if job["industry"] == industry]
     return job_list
 
 
@@ -36,8 +36,8 @@ def get_max_salary(path):  # ok
     all_salaries = set()
 
     for job in all_jobs_list:
-        if job['max_salary'] != '' and job['max_salary'] != 'invalid':
-            all_salaries.add(int(job['max_salary']))
+        if job["max_salary"] != "" and job["max_salary"] != "invalid":
+            all_salaries.add(int(job["max_salary"]))
     result = sorted(all_salaries)[-1]
     return result
 
@@ -47,36 +47,27 @@ def get_min_salary(path):  # ok
     all_salaries = set()
 
     for job in all_jobs_list:
-        if job['min_salary'] != '' and job['min_salary'] != 'invalid':
-            all_salaries.add(int(job['min_salary']))
+        if job["min_salary"] != "" and job["min_salary"] != "invalid":
+            all_salaries.add(int(job["min_salary"]))
     result = sorted(all_salaries)[0]
     return result
 
 
-def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
+def matches_salary_range(job, salary):  # ok
+    if "max_salary" not in job or "min_salary" not in job:
+        raise ValueError("Chave min_salary ou max_salary não existem")
+    elif (
+        not isinstance(job["min_salary"], int)
+        or not isinstance(job["max_salary"], int)
+        or not isinstance(salary, int)
+    ):
+        raise ValueError("Não foi passado um valor valido")
+    elif job["max_salary"] < job["min_salary"]:
+        raise ValueError("max_salary é menor que min_salary")
+    elif job["max_salary"] >= salary >= job["min_salary"]:
+        return True
+    else:
+        return False
 
 
 def filter_by_salary_range(jobs, salary):
