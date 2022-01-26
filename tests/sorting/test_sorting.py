ff08a4d21a5923cf57dc4ca1c0bafa9d4a467395
DarioJunior <dario.junior.rs@gmail.com>
2022-01-26 17:17:20 -0300

    Altera a ordem dos registros do minimo salario

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -10,33 +10,33 @@ def test_sort_by_criteria():
         {'min_salary': 101, 'max_salary': 50000, 'date_posted': '2019-09-19'},
     ]
 
-    ordened_job_by_min_salary = [{
-        {'min_salary': '', 'max_salary': '', 'date_posted': ''},
+    ordened_job_by_min_salary = [
         {'min_salary': 0, 'max_salary': 5, 'date_posted': '2020-10-20'},
         {'min_salary': 100, 'max_salary': 1000, 'date_posted': '2022-02-02'},
         {'min_salary': 101, 'max_salary': 50000, 'date_posted': '2019-09-19'},
         {'min_salary': 1000, 'max_salary': 2000, 'date_posted': '2021-12-15'},
-    }]
+        {'min_salary': '', 'max_salary': '', 'date_posted': ''},
+    ]
     sort_by(unordened_jobs, "min_salary")
     assert unordened_jobs == ordened_job_by_min_salary
 
-    ordened_job_by_max_salary = [{
+    ordened_job_by_max_salary = [
         {'min_salary': 101, 'max_salary': 50000, 'date_posted': '2019-09-19'},
         {'min_salary': 1000, 'max_salary': 2000, 'date_posted': '2021-12-15'},
         {'min_salary': 100, 'max_salary': 1000, 'date_posted': '2022-02-02'},
         {'min_salary': 0, 'max_salary': 5, 'date_posted': '2020-10-20'},
         {'min_salary': '', 'max_salary': '', 'date_posted': ''},
-    }]
+    ]
     sort_by(unordened_jobs, "max_salary")
     assert unordened_jobs == ordened_job_by_max_salary
 
-    ordened_job_by_date_posted = [{
+    ordened_job_by_date_posted = [
         {'min_salary': 100, 'max_salary': 1000, 'date_posted': '2022-02-02'},
         {'min_salary': 1000, 'max_salary': 2000, 'date_posted': '2021-12-15'},
         {'min_salary': 0, 'max_salary': 5, 'date_posted': '2020-10-20'},
         {'min_salary': 101, 'max_salary': 50000, 'date_posted': '2019-09-19'},
         {'min_salary': '', 'max_salary': '', 'date_posted': ''},
-    }]
+    ]
 
     sort_by(unordened_jobs, "date_posted")
     assert unordened_jobs == ordened_job_by_date_posted
