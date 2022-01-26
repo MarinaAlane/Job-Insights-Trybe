7845d48ef70b6d0d2e9b02bc4907ec997d5d47e1
heyset <c.inaciomatheus@gmail.com>
2022-01-26 19:00:59 -0300

    req-10: corrige path de arquivos e adiciona fixtures no pytest

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,32 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+from pytest import raises, fixture
+import src.jobs as jobs
 
 
-def test_sort_by_criteria():
+@fixture
+def jobs__unsorted():
+    return jobs.read('tests/sorting/fixtures/jobs__unsorted.csv')
+
+
+@fixture
+def jobs__sorted_by_max_salary():
+    return jobs.read('tests/sorting/fixtures/jobs__sorted_by_max_salary.csv')
+
+
+@fixture
+def jobs__sorted_by_min_salary():
+    return jobs.read('tests/sorting/fixtures/jobs__sorted_by_min_salary.csv')
+
+
+@fixture
+def jobs__sorted_by_date_posted():
+    return jobs.read('tests/sorting/fixtures/jobs__sorted_by_date_posted.csv')
+
+
+def test_sort_by_criteria(jobs__unsorted,
+                          jobs__sorted_by_max_salary,
+                          jobs__sorted_by_min_salary,
+                          jobs__sorted_by_date_posted):
+    
+
     pass
