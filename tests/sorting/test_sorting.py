4cc9067ca75995f26137017b7c5184f5b2a8c767
Breno Rodrigues <brenorm@gmail.com>
2022-01-25 23:29:09 -0300

    Requisito 10: Testa se critério inválido passado para função sort_by gera excessão

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,7 @@
-# from src.sorting import sort_by
+import pytest
+from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    with pytest.raises(ValueError, match="invalid sorting criteria: invalid_criteria"):
+        sort_by(jobs=[], criteria="invalid_criteria")
