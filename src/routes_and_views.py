c84a7cb9d5b4264fe70d0dd5f7f1de171860cbb7
Samuel Silva Melo <samuel.silva.melo@hotmail.com>
2022-01-26 20:47:14 -0300

    FIX: path read all jobs

--- src/routes_and_views.py
+++ src/routes_and_views.py
@@ -11,8 +11,12 @@ from .insights import (
     get_min_salary,
     get_max_salary,
 )
-from .more_insights import slice_jobs, get_int_from_args, build_jobs_urls
-from src.more_insights import get_job
+from .more_insights import (
+    slice_jobs,
+    get_int_from_args,
+    build_jobs_urls,
+    get_job
+)
 
 
 bp = Blueprint("client", __name__, template_folder="templates")
@@ -63,7 +67,7 @@ def list_jobs():
 
 @bp.route("/job/<index>")
 def job(index):
-    jobs = read(path="src/jobs.csv")
+    jobs = read("src/jobs.csv")
     job = get_job(jobs, index)
 
     return render_template("job.jinja2", job=job)
