fb7209a0a1eec0c90db2bb85a3f3910a85668018
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2022-01-27 13:38:35 -0300

    Requisito 01-Feature: Adicinoando leitura do arquivo csv e manipulando para criar lista de dicionarios

--- src/jobs.py
+++ src/jobs.py
@@ -1,19 +1,20 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    # Iniciando projeto
-    """Reads a file from a given path and returns its contents
+    jobs_list = []
 
-    Parameters
-    ----------
-    path : str
-        Full path to file
+    with open(path) as file:
+        info_jobs = csv.reader(file)
+        header, *data = info_jobs
 
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+        for row in data:
+            new_dict = dict()
+            for index in range(len(header)):
+                new_dict[header[index]] = row[index]
+
+            jobs_list.append(new_dict)
+
+    return jobs_list
