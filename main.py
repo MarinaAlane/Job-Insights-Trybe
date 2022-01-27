a681949e2b9a9bda136127b808eff9c61393a7a8
Marconi <marconieng@gmail.com>
2022-01-27 10:19:27 -0300

    Adiciona requisito 3

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from src.jobs import read
-from src.insights import get_unique_job_types, get_unique_industries
+from src.insights import (
+    get_unique_job_types,
+    get_unique_industries,
+    get_max_salary
+)
 
 read("src/jobs.csv")
 
 get_unique_job_types("src/jobs.csv")
 
 get_unique_industries("src/jobs.csv")
+
+get_max_salary("src/jobs.csv")
--- src/insights.py
+++ src/insights.py
@@ -59,21 +59,12 @@ def filter_by_industry(jobs, industry):
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    pass
+    jobs = read(path)
+    higher_salary = set()
+    for salary in jobs:
+        if salary['max_salary'].isdigit():
+            higher_salary.add(int((salary['max_salary'])))
+    return max(list(higher_salary))
 
 
 def get_min_salary(path):
