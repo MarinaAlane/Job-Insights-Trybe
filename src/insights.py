94851f35f76f953ce4f09b4c9ae5a8bfa7d3a47a
Ivaldo Souza <ivaldo.souza@datapar.com.br>
2022-01-26 21:04:07 -0300

    req3

--- src/insights.py
+++ src/insights.py
@@ -22,7 +22,8 @@ def get_unique_industries(path):
     job_industry_from_csv = set()
 
     for job in jobs_industries_csv:
-        job_industry_from_csv.add(job["industry"])
+        if job["insdustry"] not in job_industry_from_csv and job["industry"] != "":
+            job_industry_from_csv.append(job["industry"])
 
     return job_industry_from_csv
 
@@ -56,44 +57,8 @@ def get_min_salary(path):
 
 
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
+    jobs_salaries = read(job, salary)
 
 
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    return []
+    
