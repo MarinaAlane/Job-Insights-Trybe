71683242d6b17e7af752dfa79d1c676d2cda412c
Maria Lisboa <marialisboa1515@gmail.com>
2022-01-27 00:40:21 -0300

    req1

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,13 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    lista_csv = None
+    rows = []
+    with open(path, newline='') as csvfile:
+        lista_csv = csv.DictReader(csvfile)
+        for row in lista_csv:
+            rows.append(row)
+    return rows
