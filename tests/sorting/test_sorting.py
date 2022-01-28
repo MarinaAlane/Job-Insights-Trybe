6a26d4a517ce022542cb672cc3236aacf1c27ec1
holistic <paulofreire.si@outlook.com>
2022-01-28 13:39:22 -0300

    teste

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -15,13 +15,13 @@ min_salary_mock = [
 
 max_salary_mock = [
       {"min_salary": 4000, "max_salary": 9000, "date_posted": "2022-01-28"},
-      {"min_salary": 6000, "max_salary": 5000, "date_posted": "2022-02-28"},
+      {"min_salary": 6000, "max_salary": 8000, "date_posted": "2022-02-28"},
       {"min_salary": 5000, "max_salary": 7000, "date_posted": "2022-03-28"},
 ]
 
 date_posted_mock = [
       {"min_salary": 5000, "max_salary": 7000, "date_posted": "2022-03-28"},
-      {"min_salary": 6000, "max_salary": 5000, "date_posted": "2022-02-28"},
+      {"min_salary": 6000, "max_salary": 8000, "date_posted": "2022-02-28"},
       {"min_salary": 4000, "max_salary": 9000, "date_posted": "2022-01-28"},
 ]
 
