ffdb7f0f9a0e3f6e1791365daeace45896250057
tonpasso <wellpasso@gmail.com>
2022-01-28 19:02:47 -0300

    adiciona teste para data_posted e criterio errado

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,31 +1,44 @@
-# import pytest
+import pytest
 from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
     fake_list = [
-        {"date_posted": "2020-13-5", "max_salary": 3000, "min_salary": 2000},
-        {"date_posted": "2020-12-5", "max_salary": 2000, "min_salary": 1000},
-        {"date_posted": "2020-15-5", "max_salary": 5000, "min_salary": 4000},
-        {"date_posted": "2020-14-5", "max_salary": 4000, "min_salary": 3000},
+        {"date_posted": "2020-03-05", "max_salary": 3000, "min_salary": 2000},
+        {"date_posted": "2020-02-05", "max_salary": 2000, "min_salary": 1000},
+        {"date_posted": "2020-01-05", "max_salary": 5000, "min_salary": 4000},
+        {"date_posted": "2020-04-05", "max_salary": 4000, "min_salary": 3000},
     ]
 
     model_list_min_salary = [
-        {"date_posted": "2020-12-5", "max_salary": 2000, "min_salary": 1000},
-        {"date_posted": "2020-13-5", "max_salary": 3000, "min_salary": 2000},
-        {"date_posted": "2020-14-5", "max_salary": 4000, "min_salary": 3000},
-        {"date_posted": "2020-15-5", "max_salary": 5000, "min_salary": 4000},
+        {"date_posted": "2020-02-05", "max_salary": 2000, "min_salary": 1000},
+        {"date_posted": "2020-03-05", "max_salary": 3000, "min_salary": 2000},
+        {"date_posted": "2020-04-05", "max_salary": 4000, "min_salary": 3000},
+        {"date_posted": "2020-01-05", "max_salary": 5000, "min_salary": 4000},
     ]
 
     model_list_max_salary = [
-        {"date_posted": "2020-15-5", "max_salary": 5000, "min_salary": 4000},
-        {"date_posted": "2020-14-5", "max_salary": 4000, "min_salary": 3000},
-        {"date_posted": "2020-13-5", "max_salary": 3000, "min_salary": 2000},
-        {"date_posted": "2020-12-5", "max_salary": 2000, "min_salary": 1000},
+        {"date_posted": "2020-01-05", "max_salary": 5000, "min_salary": 4000},
+        {"date_posted": "2020-04-05", "max_salary": 4000, "min_salary": 3000},
+        {"date_posted": "2020-03-05", "max_salary": 3000, "min_salary": 2000},
+        {"date_posted": "2020-02-05", "max_salary": 2000, "min_salary": 1000},
     ]
+
+    model_data_posted = [
+        {"date_posted": "2020-04-05", "max_salary": 4000, "min_salary": 3000},
+        {"date_posted": "2020-03-05", "max_salary": 3000, "min_salary": 2000},
+        {"date_posted": "2020-02-05", "max_salary": 2000, "min_salary": 1000},
+        {"date_posted": "2020-01-05", "max_salary": 5000, "min_salary": 4000},
+    ]
+
+    sort_by(fake_list, "max_salary")
+    assert fake_list == model_list_max_salary
 
     sort_by(fake_list, "min_salary")
     assert fake_list == model_list_min_salary
 
-    sort_by(fake_list, "max_salary")
-    assert fake_list == model_list_max_salary
+    sort_by(fake_list, "date_posted")
+    assert fake_list == model_data_posted
+
+    with pytest.raises(ValueError):
+        sort_by(fake_list, "qualquer criterio")
