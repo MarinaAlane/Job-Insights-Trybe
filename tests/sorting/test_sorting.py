53d7e6c16969ed6f2153562bb6a94ec33ab5636d
holistic <paulofreire.si@outlook.com>
2022-01-28 12:35:16 -0300

    test: test_sort_by - min_salary

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,30 @@
-# from src.sorting import sort_by
+import pytest
+from src.sorting import sort_by
 
+mockingbird = [
+    {"min_salary": 5000, "max_salary": 7000, "date_posted": "2022-03-28"},
+    {"min_salary": 4000, "max_salary": 9000, "date_posted": "2022-01-28"},
+    {"min_salary": 6000, "max_salary": 5000, "date_posted": "2022-02-28"},
+]
 
-def test_sort_by_criteria():
-    pass
+min_salary_mock = [
+    {"min_salary": 4000, "max_salary": 9000, "date_posted": "2022-01-28"},
+    {"min_salary": 5000, "max_salary": 7000, "date_posted": "2022-03-28"},
+    {"min_salary": 6000, "max_salary": 5000, "date_posted": "2022-02-28"},
+]
+
+max_salary_mock = [
+      {"min_salary": 4000, "max_salary": 9000, "date_posted": "2022-01-28"},
+      {"min_salary": 6000, "max_salary": 5000, "date_posted": "2022-02-28"},
+      {"min_salary": 5000, "max_salary": 7000, "date_posted": "2022-03-28"},
+]
+
+date_posted_mock = [
+      {"min_salary": 5000, "max_salary": 7000, "date_posted": "2022-03-28"},
+      {"min_salary": 6000, "max_salary": 5000, "date_posted": "2022-02-28"},
+      {"min_salary": 4000, "max_salary": 9000, "date_posted": "2022-01-28"},
+]
+
+def test_sort_by():
+    sort_by(mockingbird, "min_salary")
+    assert mockingbird == min_salary_mock
