df6c7668273e68f666efac60021314475e9afcde
Italo <italomatos764@gmail.com>
2022-01-28 01:14:14 -0400

    Req 1

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,9 @@
 from functools import lru_cache
+from csv import DictReader
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    with open(path, "r") as jobs_file_csv:
+        jobs_list = [row for row in DictReader(jobs_file_csv)]
+    return jobs_list
