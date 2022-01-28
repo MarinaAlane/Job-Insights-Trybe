eb2a275fdf0666cae885b735e05ff93d94db38e5
guinhoanjos <iago.anjos@hotmail.com>
2022-01-28 16:55:15 -0300

    :heavy_check_mark: feat: requisito 7

--- src/insights.py
+++ src/insights.py
@@ -30,21 +30,11 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    jobs_lists = []
+    for row in jobs:
+        if row['industry'] == industry:
+            jobs_lists.append(row)
+    return jobs_lists
 
 
 def get_max_salary(path):
