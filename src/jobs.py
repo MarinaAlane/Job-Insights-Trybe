7fcc922f5a294f7be0ffc69ce49f335b67534833
Reginara Gomes <reginara.gomes@datasprints.com>
2022-01-27 22:49:23 -0300

    Adiciona req 1 - função read

--- src/jobs.py
+++ src/jobs.py
@@ -1,7 +1,10 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
 
-    return []
+    with open(path) as file:
+        dict_list = csv.DictReader(file, delimiter=",", quotechar='"')
+        return [row for row in dict_list]
