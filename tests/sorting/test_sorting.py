877f472a14301ccb5c9030818f538f5c0b4b2f3e
Thiago Leite <leite.tjs@gmail.com>
2022-01-28 17:48:58 -0300

    feat: requirement 10 working

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,86 @@
-# from src.sorting import sort_by
+import pytest
+from src.sorting import sort_by
+
+jobs_list = [
+    {
+     'date_posted': '2020-01-28',
+     'max_salary': '100000',
+     'min_salary': '10000',
+    },
+    {
+     'max_salary': '200000',
+     'date_posted': '2019-01-28',
+     'min_salary': '20000',
+    },
+    {
+     'min_salary': '30000',
+     'max_salary': '300000',
+     'date_posted': '2021-01-28'
+    },
+]
+
+max_salary_list = [
+    {
+     'max_salary': '300000',
+     'min_salary': '30000',
+     'date_posted': '2021-01-28'
+    },
+    {
+     'max_salary': '200000',
+     'date_posted': '2019-01-28',
+     'min_salary': '20000',
+    },
+    {
+     'max_salary': '100000',
+     'date_posted': '2020-01-28',
+     'min_salary': '10000',
+    },
+]
+
+min_salary_list = [
+    {
+     'min_salary': '10000',
+     'max_salary': '100000',
+     'date_posted': '2020-01-28',
+    },
+    {
+     'min_salary': '20000',
+     'max_salary': '200000',
+     'date_posted': '2019-01-28',
+    },
+    {
+     'min_salary': '30000',
+     'max_salary': '300000',
+     'date_posted': '2021-01-28'
+    },
+]
+
+date_posted_list = [
+    {
+     'date_posted': '2021-01-28',
+     'max_salary': '300000',
+     'min_salary': '30000',
+    },
+    {
+     'date_posted': '2020-01-28',
+     'max_salary': '100000',
+     'min_salary': '10000',
+    },
+    {
+     'date_posted': '2019-01-28',
+     'max_salary': '200000',
+     'min_salary': '20000',
+    },
+]
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(jobs_list, "max_salary")
+    assert jobs_list == max_salary_list
+    sort_by(jobs_list, "min_salary")
+    assert jobs_list == min_salary_list
+    sort_by(jobs_list, "date_posted")
+    assert jobs_list == date_posted_list
+
+    with pytest.raises(ValueError, match='invalid sorting criteria: criteria'):
+        sort_by(jobs_list, 'criteria')
