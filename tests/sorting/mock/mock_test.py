dfd49bc5a58d78f34094f52b53474e0a78735b1b
Bernardo Prado <bernardoprado22@yahoo.com.br>
2022-01-28 19:35:46 -0300

    Somente um teste dos testes e de mock

---
+++ tests/sorting/mock/mock_test.py
@@ -0,0 +1,5 @@
+mock_ = [
+    {"min_salary": 3000, "max_salary": 8000, "date_posted": "2020-03-01"},
+    {"min_salary": 2000, "max_salary": 7000, "date_posted": "2020-01-01"},
+    {"min_salary": 1000, "max_salary": 2000, "date_posted": "2020-02-01"},
+]
--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,30 @@
-# from src.sorting import sort_by
+import pytest
+from src.sorting import sort_by
+from .mock import mock_
 
 
 def test_sort_by_criteria():
     pass
+    sort_by(mock_, "date_posted")
+    assert mock_ == [
+        {"min_salary": 8000, "max_salary": 9000, "date_posted": "2022-04-22"},
+        {"min_salary": 7000, "max_salary": 8000, "date_posted": "2019-12-30"},
+        {"min_salary": 0, "max_salary": 2000, "date_posted": "2020-08-07"},
+    ]
+
+    sort_by(mock_, "max_salary")
+    assert mock_ == [
+        {"min_salary": 8000, "max_salary": 9000, "date_posted": "2022-04-22"},
+        {"min_salary": 7000, "max_salary": 8000, "date_posted": "2019-12-30"},
+        {"min_salary": 0, "max_salary": 2000, "date_posted": "2020-08-07"},
+    ]
+
+    sort_by(mock_, "min_salary")
+    assert mock_ == [
+        {"min_salary": 1000, "max_salary": 9000, "date_posted": "2022-04-22"},
+        {"min_salary": 2000, "max_salary": 8000, "date_posted": "2019-12-30"},
+        {"min_salary": 3000, "max_salary": 4000, "date_posted": "2020-08-07"},
+    ]
+
+    with pytest.raises(ValueError):
+        sort_by(mock_, "fail criteria")
