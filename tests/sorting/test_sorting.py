881796c3bb65baf2a29398952a827a44f0330506
Bernardo Prado <bernardoprado22@yahoo.com.br>
2022-01-29 19:12:50 -0300

    brincando com os testes

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -5,25 +5,25 @@ from src.sorting import sort_by
 
 def test_sort_by_criteria():
     pass
+    sort_by(mock_t, "min_salary")
+    assert mock_t == [
+        {"min_salary": 1000, "max_salary": 2000, "date_posted": "2020-01-01"},
+        {"min_salary": 2000, "max_salary": 7000, "date_posted": "2020-02-01"},
+        {"min_salary": 3000, "max_salary": 8000, "date_posted": "2020-03-01"},
+    ]
+
     sort_by(mock_t, "max_salary")
     assert mock_t == [
-        {"min_salary": 8000, "max_salary": 9000, "date_posted": "2022-04-22"},
-        {"min_salary": 7000, "max_salary": 8000, "date_posted": "2019-12-30"},
-        {"min_salary": 0, "max_salary": 2000, "date_posted": "2020-08-07"},
+        {"min_salary": 3000, "max_salary": 8000, "date_posted": "2020-03-01"},
+        {"min_salary": 2000, "max_salary": 7000, "date_posted": "2020-02-01"},
+        {"min_salary": 1000, "max_salary": 2000, "date_posted": "2020-01-01"},
     ]
 
     sort_by(mock_t, "date_posted")
     assert mock_t == [
         {"min_salary": 3000, "max_salary": 8000, "date_posted": "2020-03-01"},
-        {"min_salary": 1000, "max_salary": 2000, "date_posted": "2020-02-01"},
-        {"min_salary": 7000, "max_salary": 8000, "date_posted": "2020-01-01"},
-    ]
-
-    sort_by(mock_t, "min_salary")
-    assert mock_t == [
-        {"min_salary": 1000, "max_salary": 9000, "date_posted": "2022-04-22"},
-        {"min_salary": 2000, "max_salary": 8000, "date_posted": "2019-12-30"},
-        {"min_salary": 3000, "max_salary": 4000, "date_posted": "2020-08-07"},
+        {"min_salary": 2000, "max_salary": 7000, "date_posted": "2020-02-01"},
+        {"min_salary": 1000, "max_salary": 2000, "date_posted": "2020-01-01"},
     ]
 
     with pytest.raises(ValueError):
