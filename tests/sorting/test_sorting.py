b88399966ff0267f8ff8e975a11f36933ce242d1
lucasSCsantos <luc.cristovam10@gmail.com>
2022-01-29 16:27:50 -0300

    dates

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -2,27 +2,27 @@ from src.sorting import sort_by
 import pytest
 
 jobs = [
-    {"date_posted": "2002-08-22", "max_salary": 3000, "min_salary": 5000},
-    {"date_posted": "2003-09-23", "max_salary": 2000, "min_salary": 4000},
-    {"date_posted": "2005-08-22", "max_salary": 1000, "min_salary": 2000},
+    {"date_posted": "2001-08-03", "max_salary": 3000, "min_salary": 5000},
+    {"date_posted": "2016-08-16", "max_salary": 2000, "min_salary": 4000},
+    {"date_posted": "2020-12-27", "max_salary": 1000, "min_salary": 2000},
 ]
 
 max_salary = [
-    {"date_posted": "2002-08-22", "max_salary": 3000, "min_salary": 5000},
-    {"date_posted": "2003-09-23", "max_salary": 2000, "min_salary": 4000},
-    {"date_posted": "2005-08-22", "max_salary": 1000, "min_salary": 2000},
+    {"date_posted": "2001-08-03", "max_salary": 3000, "min_salary": 5000},
+    {"date_posted": "2016-08-16", "max_salary": 2000, "min_salary": 4000},
+    {"date_posted": "2020-12-27", "max_salary": 1000, "min_salary": 2000},
 ]
 
 min_salary = [
-    {"date_posted": "2005-08-22", "max_salary": 1000, "min_salary": 2000},
-    {"date_posted": "2003-09-23", "max_salary": 2000, "min_salary": 4000},
-    {"date_posted": "2002-08-22", "max_salary": 3000, "min_salary": 5000},
+    {"date_posted": "2020-12-27", "max_salary": 1000, "min_salary": 2000},
+    {"date_posted": "2016-08-16", "max_salary": 2000, "min_salary": 4000},
+    {"date_posted": "2001-08-03", "max_salary": 3000, "min_salary": 5000},
 ]
 
 date_posted = [
-    {"date_posted": "2005-08-22", "max_salary": 1000, "min_salary": 2000},
-    {"date_posted": "2003-09-23", "max_salary": 2000, "min_salary": 4000},
-    {"date_posted": "2002-08-22", "max_salary": 3000, "min_salary": 5000},
+    {"date_posted": "2020-12-27", "max_salary": 1000, "min_salary": 2000},
+    {"date_posted": "2016-08-16", "max_salary": 2000, "min_salary": 4000},
+    {"date_posted": "2001-08-03", "max_salary": 3000, "min_salary": 5000},
 ]
 
 
