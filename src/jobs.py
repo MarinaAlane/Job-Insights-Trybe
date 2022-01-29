baafde25185aba2b5a01983d7764e852f57787f5
Maran <maranmorimoto@gmail.com>
2022-01-29 18:36:13 -0300

    req01 - implement read function

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,26 @@
 from functools import lru_cache
+import csv  # importo a livraria CSV pq o arquivo que quero ler é do tipo CSV
 
 
 @lru_cache
-def read(path):
-    """Reads a file from a given path and returns its contents
+def read(file_path):
+    with open(file_path, 'r') as csv_file:
+        jobs_csv = csv.DictReader(csv_file)
+        jobs = []
+        for row in jobs_csv:
+            jobs.append(row)
+        return jobs
 
-    Parameters
-    ----------
-    path : str
-        Full path to file
 
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+# a função read deve abrir um arquivo CV e retornar dados em formato de list
+# de dicionários
+
+# o with serve para nao precisarmos ficar fechando o arquivo e
+# e ficarmos ocupando mta memória. Então com with o arquivo abre
+# faz o que necessita e fecha.
+
+# o csv.DictReader serve para retonar dados em formato de lista
+# de dicionários
+
+# o for lê-se: para cada linha de jobs_csv, adiciono na variável jobs
+# cada linha e retorno jobs para a função.
