9d570a4eb0d940a15da2ee97cccde1ebb2f35c87
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2022-01-29 15:11:23 -0300

    test: ensure sort_by sort with given criteria

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,44 @@
-# from src.sorting import sort_by
+import pytest
+from src.sorting import sort_by
+
+
+# @pytest.fixture
+def jobs_list():
+    return [
+        {"min_salary": 2000, "max_salary": 3000, "data-posted": "2022-01-27"},
+        {"min_salary": 1100, "max_salary": 2500, "data-posted": "2022-02-27"},
+        {"min_salary": 2100, "max_salary": 2200, "data-posted": "2022-01-01"},
+    ]
+
+
+ordered_by_date = [
+    {"min_salary": 2100, "max_salary": 2200, "data-posted": "2022-01-01"},
+    {"min_salary": 2000, "max_salary": 3000, "data-posted": "2022-01-27"},
+    {"min_salary": 1100, "max_salary": 2500, "data-posted": "2022-02-27"},
+]
+
+ordered_by_max_salary = [
+    {"min_salary": 2000, "max_salary": 3000, "data-posted": "2022-01-27"},
+    {"min_salary": 1100, "max_salary": 2500, "data-posted": "2022-02-27"},
+    {"min_salary": 2100, "max_salary": 2200, "data-posted": "2022-01-01"},
+]
+
+ordered_by_min_salary = [
+    {"min_salary": 1100, "max_salary": 2500, "data-posted": "2022-02-27"},
+    {"min_salary": 2000, "max_salary": 3000, "data-posted": "2022-01-27"},
+    {"min_salary": 2100, "max_salary": 2200, "data-posted": "2022-01-01"},
+]
 
 
 def test_sort_by_criteria():
-    pass
+    jobs = jobs_list()
+    sort_by(jobs, "date_posted")
+    assert jobs == ordered_by_date
+
+    jobs = jobs_list()
+    sort_by(jobs, "max_salary")
+    assert jobs == ordered_by_max_salary
+
+    jobs = jobs_list()
+    sort_by(jobs, "min_salary")
+    assert jobs == ordered_by_min_salary
