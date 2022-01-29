17f870fcc587b6ae2fcc4b460f0fd7fcaaedb9ac
anagiorgiani <cavalheiro.anacarolina@gmail.com>
2022-01-29 15:48:37 -0300

    desafio 1

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,14 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
 
-    Parameters
-    ----------
-    path : str
-        Full path to file
+    lista = []
+    with open(path) as file:
+        list = csv.DictReader(file, delimiter=",", quotechar='"')
+        for row in list:
+            lista.append(row)
 
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    return lista
