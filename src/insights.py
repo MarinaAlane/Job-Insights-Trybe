a6f62215e9b0a21eb0c58c1aa254d352660098fc
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2022-01-29 18:45:57 -0300

    req3

--- src/insights.py
+++ src/insights.py
@@ -30,21 +30,14 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
+    industries = read(path)
+    list_industries = list()
 
-    Must call `read`
+    for industry in industries:
+        if not industry['industry'] in list_industries and industry['industry'] != '':
+            list_industries.append(industry['industry'])
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    return list_industries
 
 
 def filter_by_industry(jobs, industry):
