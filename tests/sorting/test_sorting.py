a48dd30a8e3bca7b8365d3df6bc9d02d7b735af4
lucasSCsantos <luc.cristovam10@gmail.com>
2022-01-29 16:20:54 -0300

    inhame

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,40 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+import pytest
+
+jobs = [
+    { "date_posted": "2002-08-22", "max_salary": 3000, "min_salary": 5000 },
+    { "date_posted": "2003-09-23", "max_salary": 2000, "min_salary": 4000 },
+    { "date_posted": "2005-08-22", "max_salary": 1000, "min_salary": 2000 },
+]
+
+max_salary = [
+    { "date_posted": "2002-08-22", "max_salary": 3000, "min_salary": 5000 },
+    { "date_posted": "2003-09-23", "max_salary": 2000, "min_salary": 4000 },
+    { "date_posted": "2005-08-22", "max_salary": 1000, "min_salary": 2000 },
+]
+
+min_salary = [
+    { "date_posted": "2005-08-22", "max_salary": 1000, "min_salary": 2000 },
+    { "date_posted": "2003-09-23", "max_salary": 2000, "min_salary": 4000 },
+    { "date_posted": "2002-08-22", "max_salary": 3000, "min_salary": 5000 },
+]
+
+date_posted = [
+    { "date_posted": "2005-08-22", "max_salary": 1000, "min_salary": 2000 },
+    { "date_posted": "2003-09-23", "max_salary": 2000, "min_salary": 4000 },
+    { "date_posted": "2002-08-22", "max_salary": 3000, "min_salary": 5000 },
+]
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(jobs, "max_salary")
+    assert jobs == max_salary
+
+    sort_by(jobs, "min_salary")
+    assert jobs == min_salary
+
+    sort_by(jobs, "date_posted")
+    assert jobs == date_posted
+
+    with pytest.raises(ValueError, match="invalid sorting criteria: date_salary"):
+        sort_by(jobs, "date_salary")
