d5206c2e639a93136f981d96815dfe9306ac18aa
Maria Lisboa <marialisboa1515@gmail.com>
2022-01-28 22:35:14 -0300

    add req4

--- src/insights.py
+++ src/insights.py
@@ -61,20 +61,14 @@ def filter_by_industry(jobs, industry):
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
+    valore_unicos = read(path)
+    max_s = set()
 
-    Must call `read`
+    for row in valore_unicos:
+        if row['max_salary'] != '' and row['max_salary'] != 'invalid':
+            max_s.add(int(row['max_salary']))
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
+    return max(max_s)
     pass
 
 
