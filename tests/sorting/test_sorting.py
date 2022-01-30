cbd335ea6ce48195b226457497867b0a55982710
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2022-01-30 16:11:15 -0300

    req10

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,13 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    jobs = [{"min_salary": 1000}, {"min_salary": 2000}, {"min_salary": 500}]
+
+    sort_by(jobs, "min_salary")
+
+    assert jobs == [
+        {"min_salary": 500},
+        {"min_salary": 1000},
+        {"min_salary": 2000},
+    ]
