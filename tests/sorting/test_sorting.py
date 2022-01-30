eb03cf9281376c5161ff0e6174b5c52e02782039
Víctor Cançado <victor.cancado@gmail.com>
2022-01-30 14:15:30 -0300

    Requisito 10.1 ok

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,19 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    jobs = [
+        {"min_salary": 1500},
+        {"min_salary": 2100},
+        {"min_salary": 1200},
+        {"min_salary": 1800},
+        {"min_salary": 1100},
+    ]
+    sort_by(jobs, "min_salary")
+    assert jobs == [
+        {"min_salary": 1100},
+        {"min_salary": 1200},
+        {"min_salary": 1500},
+        {"min_salary": 1800},
+        {"min_salary": 2100},
+    ]
