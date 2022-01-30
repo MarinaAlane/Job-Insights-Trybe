85a795918df0aed97ba16e8fa99af8976884f24e
Maran <maranmorimoto@gmail.com>
2022-01-29 21:30:50 -0300

    req3 - implement get_unique_industries function

--- src/insights.py
+++ src/insights.py
@@ -38,22 +38,18 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
+    jobs_list = jobs.read(path)
+    ind_types = []
+    for job in jobs_list:
+        if job['industry']:
+            ind_types.append(job['industry'])
+    return list(set(ind_types))
 
-    Must call `read`
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
-
+# a função get_unique_industries le o arquivo CSV
+# e coloca na variável ind_types os tipos de industrias encontradas
+# na lista. O if questiona se o job[industry] é vazio ou nao e se nao
+# adiciona-o na vriavel, retornando os tipos cadastrados.
 
 def filter_by_industry(jobs, industry):
     """Filters a list of jobs by industry
