12c6c1641aa518890b2686e8dffce7762ce8d146
Marina Alane <marina.alane@outlook.com>
2022-01-30 20:45:18 -0300

    fix: req 10

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,42 +1,44 @@
 from src.sorting import sort_by
+import pytest
 
-
-def jobs_list():
-    return [
-        {"min_salary": 2000, "max_salary": 3000, "data-posted": "2022-01-27"},
-        {"min_salary": 1100, "max_salary": 2500, "data-posted": "2022-02-27"},
-        {"min_salary": 2100, "max_salary": 2200, "data-posted": "2022-01-01"},
-    ]
-
-
-ordered_by_date = [
-    {"min_salary": 2100, "max_salary": 2200, "data-posted": "2022-01-01"},
-    {"min_salary": 2000, "max_salary": 3000, "data-posted": "2022-01-27"},
-    {"min_salary": 1100, "max_salary": 2500, "data-posted": "2022-02-27"},
-]
-
-ordered_by_max_salary = [
-    {"min_salary": 2000, "max_salary": 3000, "data-posted": "2022-01-27"},
-    {"min_salary": 1100, "max_salary": 2500, "data-posted": "2022-02-27"},
-    {"min_salary": 2100, "max_salary": 2200, "data-posted": "2022-01-01"},
+jobs_list = [
+     {"min_salary": 20, "max_salary": 40, "date_posted": "2022-03-01"},
+     {"min_salary": 10, "max_salary": 60, "date_posted": "2022-01-01"},
+     {"min_salary": 30, "max_salary": 50, "date_posted": "2022-02-01"},
 ]
 
 ordered_by_min_salary = [
-    {"min_salary": 1100, "max_salary": 2500, "data-posted": "2022-02-27"},
-    {"min_salary": 2000, "max_salary": 3000, "data-posted": "2022-01-27"},
-    {"min_salary": 2100, "max_salary": 2200, "data-posted": "2022-01-01"},
+     {"min_salary": 10, "max_salary": 60, "date_posted": "2022-01-01"},
+     {"min_salary": 20, "max_salary": 40, "date_posted": "2022-03-01"},
+     {"min_salary": 30, "max_salary": 50, "date_posted": "2022-02-01"},
+]
+
+ordered_by_max_salary = [
+     {"min_salary": 10, "max_salary": 60, "date_posted": "2022-01-01"},
+     {"min_salary": 30, "max_salary": 50, "date_posted": "2022-02-01"},
+     {"min_salary": 20, "max_salary": 40, "date_posted": "2022-03-01"},
+]
+
+ordered_by_date = [
+     {"min_salary": 20, "max_salary": 40, "date_posted": "2022-03-01"},
+     {"min_salary": 30, "max_salary": 50, "date_posted": "2022-02-01"},
+     {"min_salary": 10, "max_salary": 60, "date_posted": "2022-01-01"},
 ]
 
 
-def test_sorting_criteria():
-    jobs = jobs_list()
-    sort_by(jobs, "date_posted")
-    assert jobs == ordered_by_date
+def test_sort_by_criteria():
+    pass
+    sort_by(jobs_list, "min_salary")
+    assert jobs_list == ordered_by_min_salary
 
-    jobs = jobs_list
-    sort_by(jobs, "max_salary")
-    assert jobs == ordered_by_max_salary
+    sort_by(jobs_list, "max_salary")
+    assert jobs_list == ordered_by_max_salary
 
-    jobs = jobs_list
-    sort_by(jobs, "min_salary")
-    assert jobs == ordered_by_min_salary
+    sort_by(jobs_list, "date_posted")
+    assert jobs_list == ordered_by_date
+
+    with pytest.raises(ValueError):
+        sort_by(jobs_list, "invalid_criteria")
+
+# ref linha 40 a 42 :
+# https://github.com/tryber/sd-011-project-job-insights/pull/132/fil
