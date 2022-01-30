9c736aa5611386309ffc80363083cf15b27caf23
Maria Lisboa <marialisboa1515@gmail.com>
2022-01-29 23:09:45 -0300

    add req8

--- src/insights.py
+++ src/insights.py
@@ -61,29 +61,20 @@ def get_min_salary(path):
 
 
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
+    if 'max_salary' not in job or 'min_salary' not in job:
+        raise ValueError('Chaves não existem')
+    elif (
+        not isinstance(job['min_salary'], int)
+        or not isinstance(job['max_salary'], int)
+        or not isinstance(salary, int)
+    ):
+        raise ValueError('Sem valor válido')
+    elif job['max_salary'] < job['min_salary']:
+        raise ValueError('max_salary é menor que min_salary')
+    elif job["max_salary"] >= salary >= job['min_salary']:
+        return True
+    else:
+        return False
 
 
 def filter_by_salary_range(jobs, salary):
