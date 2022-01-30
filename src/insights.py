be8d4e000adc7c566e3f980c05980de25c487188
OldRoge <rogeriomrjr@icloud.com>
2022-01-30 17:14:45 -0300

    requirement 4

--- src/insights.py
+++ src/insights.py
@@ -59,21 +59,13 @@ def filter_by_industry(jobs, industry):
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    pass
+    jobs_list = read(path)
+    max_salary = list()
+    for job in jobs_list:
+        if job['max_salary'].isnumeric():
+            max_salary.append(int(job['max_salary']))
+    max_salary.sort()
+    return max_salary[-1]
 
 
 def get_min_salary(path):
