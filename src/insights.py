3bb0c95481320c94f2dbe687410cdb7afe9e8f18
dcmatheus <mathws.costag@gmail.com>
2022-01-31 00:41:00 -0300

    get_unique_job_types func

--- src/insights.py
+++ src/insights.py
@@ -1,3 +1,6 @@
+from jobs import read
+
+
 def get_unique_job_types(path):
     """Checks all different job types and returns a list of them
 
@@ -13,7 +16,11 @@ def get_unique_job_types(path):
     list
         List of unique job types
     """
-    return []
+    jobs = read(path)
+    filtered_jobs = []
+    for job in jobs:
+        filtered_jobs.append(job["job_type"])
+    return filtered_jobs
 
 
 def filter_by_job_type(jobs, job_type):
