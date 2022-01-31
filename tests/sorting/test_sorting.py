532ac689e89128983944c71aa59fb2e9a89a692a
rodolfowolff <rodolfoluizwolff@gmail.com>
2022-01-31 00:05:14 -0300

    feat: req.10.2

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -20,3 +20,12 @@ def test_sort_by_criteria(jobs):
     jobs_min = jobs.copy()
     sort_by(jobs_min, "min_salary")
     assert jobs_min == order_by_min_salary
+
+    order_by_max_salary = [
+        {"min_salary": 1500, "max_salary": 25000, "date_posted": "2022-09-13"},
+        {"min_salary": 1000, "max_salary": 15000, "date_posted": "2022-07-05"},
+        {"min_salary": 500, "max_salary": 10000, "date_posted": "2022-06-27"},
+    ]
+    jobs_max = jobs.copy()
+    sort_by(jobs_max, "max_salary")
+    assert jobs_max == order_by_max_salary
