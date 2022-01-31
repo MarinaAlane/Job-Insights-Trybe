e47d2655f344814073ae047db44744bba62da267
holistic <paulofreire.si@outlook.com>
2022-01-31 08:57:26 -0300

    testing remote evaluator

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,44 +1,90 @@
-import pytest
-from copy import deepcopy
+# import pytest
+# from copy import deepcopy
+# from src.sorting import sort_by
+
+# mockingbird = [
+#     {"min_salary": 5000, "max_salary": 7000, "date_posted": "2022-03-28"},
+#     {"min_salary": 4000, "max_salary": 9000, "date_posted": "2022-01-28"},
+#     {"min_salary": 6000, "max_salary": 8000, "date_posted": "2022-02-28"},
+# ]
+
+# min_salary_mock = [
+#     {"min_salary": 4000, "max_salary": 9000, "date_posted": "2022-01-28"},
+#     {"min_salary": 5000, "max_salary": 7000, "date_posted": "2022-03-28"},
+#     {"min_salary": 6000, "max_salary": 8000, "date_posted": "2022-02-28"},
+# ]
+
+# max_salary_mock = [
+#       {"min_salary": 4000, "max_salary": 9000, "date_posted": "2022-01-28"},
+#       {"min_salary": 6000, "max_salary": 8000, "date_posted": "2022-02-28"},
+#       {"min_salary": 5000, "max_salary": 7000, "date_posted": "2022-03-28"},
+# ]
+
+# date_posted_mock = [
+#       {"min_salary": 5000, "max_salary": 7000, "date_posted": "2022-03-28"},
+#       {"min_salary": 6000, "max_salary": 8000, "date_posted": "2022-02-28"},
+#       {"min_salary": 4000, "max_salary": 9000, "date_posted": "2022-01-28"},
+# ]
+
+
+# def test_sort_by():
+#     x = deepcopy(mockingbird)
+#     sort_by(x, "min_salary")
+#     assert x == min_salary_mock
+
+#     x = deepcopy(mockingbird)
+#     sort_by(x, "max_salary")
+#     assert x == max_salary_mock
+
+#     x = deepcopy(mockingbird)
+#     sort_by(x, "date_posted")
+#     assert x == date_posted_mock
+
+#     with pytest.raises(ValueError):
+#         sort_by(mockingbird, "invalid")
+
 from src.sorting import sort_by
 
-mockingbird = [
-    {"min_salary": 5000, "max_salary": 7000, "date_posted": "2022-03-28"},
-    {"min_salary": 4000, "max_salary": 9000, "date_posted": "2022-01-28"},
-    {"min_salary": 6000, "max_salary": 8000, "date_posted": "2022-02-28"},
+array_object = [
+    {
+        "min_salary": 1000,
+        "max_salary": 3000,
+        "date_posted": "2022-03-27",
+    },
+    {
+        "min_salary": 10000,
+        "max_salary": 30000,
+        "date_posted": "2022-03-27",
+    },
+    {
+        "min_salary": 12000,
+        "max_salary": 32000,
+        "date_posted": "2022-03-27",
+    },
+    {
+        "min_salary": 23000,
+        "max_salary": 33000,
+        "date_posted": "2022-03-23",
+    },
 ]
 
-min_salary_mock = [
-    {"min_salary": 4000, "max_salary": 9000, "date_posted": "2022-01-28"},
-    {"min_salary": 5000, "max_salary": 7000, "date_posted": "2022-03-28"},
-    {"min_salary": 6000, "max_salary": 8000, "date_posted": "2022-02-28"},
+min_mock = [
+    array_object[0],
+    array_object[1],
+    array_object[2],
+    array_object[3],
 ]
-
-max_salary_mock = [
-      {"min_salary": 4000, "max_salary": 9000, "date_posted": "2022-01-28"},
-      {"min_salary": 6000, "max_salary": 8000, "date_posted": "2022-02-28"},
-      {"min_salary": 5000, "max_salary": 7000, "date_posted": "2022-03-28"},
-]
-
-date_posted_mock = [
-      {"min_salary": 5000, "max_salary": 7000, "date_posted": "2022-03-28"},
-      {"min_salary": 6000, "max_salary": 8000, "date_posted": "2022-02-28"},
-      {"min_salary": 4000, "max_salary": 9000, "date_posted": "2022-01-28"},
+max_mock = [
+    array_object[3],
+    array_object[2],
+    array_object[1],
+    array_object[0],
 ]
 
 
-def test_sort_by():
-    x = deepcopy(mockingbird)
-    sort_by(x, "min_salary")
-    assert x == min_salary_mock
+def test_sort_by_criteria():
+    sort_by(array_object, "min_salary")
+    assert array_object == min_mock
 
-    x = deepcopy(mockingbird)
-    sort_by(x, "max_salary")
-    assert x == max_salary_mock
-
-    x = deepcopy(mockingbird)
-    sort_by(x, "date_posted")
-    assert x == date_posted_mock
-
-    with pytest.raises(ValueError):
-        sort_by(mockingbird, "invalid")
+    sort_by(array_object, "max_salary")
+    assert array_object == max_mock
