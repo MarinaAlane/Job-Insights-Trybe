9dbb8a44c8211b3f8e02f526a2d7b115c3d7bb30
Italo <italomatos764@gmail.com>
2022-01-31 02:32:24 -0400

    Requisito 10

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,28 @@
-# from src.sorting import sort_by
+import pytest
+from src.sorting import sort_by
+
+
+jobs = [
+    {"max_salary": 6000, "min_salary": 3000, "date_posted": "2022-01-01"},
+    {"max_salary": 5000, "min_salary": 2000, "date_posted": "2021-09-09"},
+    {"max_salary": 4000, "min_salary": 1000, "date_posted": "2019-12-12"},
+]
 
 
 def test_sort_by_criteria():
-    pass
+    with pytest.raises(TypeError):
+        sort_by(jobs, "invalid")
+
+    sort_by(jobs, "max_salary")
+    assert jobs == [
+        {'max_salary': 6000, 'min_salary': 3000, 'date_posted': '2022-01-01'},
+        {'max_salary': 5000, 'min_salary': 2000, 'date_posted': '2021-09-09'},
+        {'max_salary': 4000, 'min_salary': 1000, 'date_posted': '2019-12-12'},
+    ]
+
+    sort_by(jobs, "min_salary")
+    assert jobs == [
+        {'max_salary': 4000, 'min_salary': 1000, 'date_posted': '2019-12-12'},
+        {'max_salary': 6000, 'min_salary': 3000, 'date_posted': '2022-01-01'},
+        {'max_salary': 5000, 'min_salary': 2000, 'date_posted': '2021-09-09'},
+    ]
