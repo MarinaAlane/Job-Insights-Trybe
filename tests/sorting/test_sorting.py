0c29c7b1b631a2862408114fe91f48c6d6c76f7c
cristian <cristian.brum2@yahoo.com.br>
2022-01-31 09:03:44 -0300

    :rocket: req 10

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,31 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
     pass
+    jobs = [
+        {"min_salary": 2500, "max_salary": 3000, "date_posted": "1992-04-29"},
+        {"min_salary": 3000, "max_salary": 4000, "date_posted": "1992-04-28"},
+        {"min_salary": 4000, "max_salary": 5000, "date_posted": "1992-04-27"},
+    ]
+
+    sort_by(jobs, "min_salary")
+    assert jobs == [
+        {"min_salary": 2500, "max_salary": 3000, "date_posted": "1992-04-29"},
+        {"min_salary": 3000, "max_salary": 4000, "date_posted": "1992-04-28"},
+        {"min_salary": 4000, "max_salary": 5000, "date_posted": "1992-04-27"},
+    ]
+
+    sort_by(jobs, "max_salary")
+    assert jobs == [
+        {"min_salary": 4000, "max_salary": 5000, "date_posted": "1992-04-27"},
+        {"min_salary": 3000, "max_salary": 4000, "date_posted": "1992-04-28"},
+        {"min_salary": 2500, "max_salary": 3000, "date_posted": "1992-04-29"},
+    ]
+
+    sort_by(jobs, "date_posted")
+    assert jobs == [
+        {"min_salary": 2500, "max_salary": 3000, "date_posted": "1992-04-29"},
+        {"min_salary": 3000, "max_salary": 4000, "date_posted": "1992-04-28"},
+        {"min_salary": 4000, "max_salary": 5000, "date_posted": "1992-04-27"},
+    ]
