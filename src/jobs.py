5db3647a0317782446230a8e1bd919b7d962b39e
Bruna Campos <92brunacampos@gmail.com>
2022-01-30 21:15:53 -0300

    finalmente sai do requisito 1

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,18 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
+    # obrigada pessoas da turma 03 e 04 que postaram o erro exception
+    # UnicodeDecodeError no slack e como resolver <3
+    with open(path, encoding='utf-8') as file:
+        jobs_list = csv.reader(file, delimiter=",", quotechar='"')
+        header, *data = jobs_list
 
-    Parameters
-    ----------
-    path : str
-        Full path to file
+        result = []
 
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+        for row in data:
+            row = dict(zip(header, row))
+            result.append(row)
+        return result
