1019c50e0884fd81eac0d4f7d5d9005c54b7085e
Igor Farias <igor.farias@liferay.com>
2022-01-31 12:21:13 -0300

    Adiciona requisito 10

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,57 @@
-# from src.sorting import sort_by
+import pytest
+from src.sorting import sort_by
 
 
-def test_sort_by_criteria():
-    pass
+@pytest.fixture
+def jobs():
+    return [
+        {"min_salary": 500, "max_salary": 1000, "date_posted": "2022-01-20"},
+        {"min_salary": 1500, "max_salary": 2000, "date_posted": "2022-01-21"},
+        {"min_salary": 2500, "max_salary": 3000, "date_posted": "2022-01-22"},
+        {"min_salary": 3500, "max_salary": 5000, "date_posted": "2022-01-23"},
+    ]
+
+
+ordered_by_min = [
+    {"min_salary": 500, "max_salary": 1000, "date_posted": "2022-01-20"},
+    {"min_salary": 1500, "max_salary": 2000, "date_posted": "2022-01-21"},
+    {"min_salary": 2500, "max_salary": 3000, "date_posted": "2022-01-22"},
+    {"min_salary": 3500, "max_salary": 5000, "date_posted": "2022-01-23"},
+]
+
+ordered_by_max = [
+    {"min_salary": 3500, "max_salary": 5000, "date_posted": "2022-01-23"},
+    {"min_salary": 2500, "max_salary": 3000, "date_posted": "2022-01-22"},
+    {"min_salary": 1500, "max_salary": 2000, "date_posted": "2022-01-21"},
+    {"min_salary": 500, "max_salary": 1000, "date_posted": "2022-01-20"},
+]
+
+ordered_by_date = [
+    {"min_salary": 3500, "max_salary": 5000, "date_posted": "2022-01-23"},
+    {"min_salary": 2500, "max_salary": 3000, "date_posted": "2022-01-22"},
+    {"min_salary": 1500, "max_salary": 2000, "date_posted": "2022-01-21"},
+    {"min_salary": 500, "max_salary": 1000, "date_posted": "2022-01-20"},
+]
+
+
+def test_sort_by_criteria(jobs):
+    sort_by(jobs, "max_salary")
+    assert jobs == ordered_by_max
+
+    sort_by(jobs, "min_salary")
+    assert jobs == ordered_by_min
+
+    sort_by(jobs, "date_posted")
+    assert jobs == ordered_by_date
+
+    with pytest.raises(ValueError, match="invalid sorting criteria: job_type"):
+        jobs_copy = jobs.copy()
+        sort_by(jobs_copy, "job_type")
+
+    invalid_criteria = ""
+
+    with pytest.raises(
+        ValueError, match=f"invalid sorting criteria: {invalid_criteria}"
+    ):
+        jobs_copy = jobs.copy()
+        sort_by(jobs_copy, invalid_criteria)
