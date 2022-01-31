6322896777962136a9013e1c896ed45328c30e49
Igor Farias <igor.farias@liferay.com>
2022-01-31 09:08:34 -0300

    Adiciona requisito 1

--- src/jobs.py
+++ src/jobs.py
@@ -1,9 +1,23 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
+
+    with open(path, "r") as file:
+        file_reader = csv.reader(file, delimiter=",", quotechar='"')
+        header, *data = file_reader
+
+    file_row_list = []
+    for row in data:
+        row_dict = dict(zip(header, row))
+        file_row_list.append(row_dict)
+
+    """
+    https://stackoverflow.com/questions/209840/how-do-i-convert-two-lists-into-a-dictionary
+    
+    Reads a file from a given path and returns its contents
 
     Parameters
     ----------
@@ -15,4 +29,4 @@ def read(path):
     list
         List of rows as dicts
     """
-    return []
+    return file_row_list
