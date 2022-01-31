d13db0e0536360289421a10367d75ef8ed4bc711
dcmatheus <mathws.costag@gmail.com>
2022-01-31 04:34:06 -0300

    criterial error

---
+++ tests/sorting/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+print(sort_by(jobs, criteria[0]))
--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,36 +1,27 @@
+import pytest
 from src.sorting import sort_by
 
-criteria = ["max_salary", "min_salary", "date_posted"]
+criteria = ["max_salary", "min_salary", "date_posted", "random"]
 
 jobs = [
-    {
-        "sort": 1,
-        "max_salary": 500,
-        "min_salary": 100,
-    },
-    {
-        "sort": 2,
-        "max_salary": 400,
-        "min_salary": 110,
-    },
-    {
-        "sort": 3,
-        "max_salary": 300,
-        "min_salary": 120,
-    },
-    {
-        "sort": 4,
-        "max_salary": 200,
-        "min_salary": 130,
-    },
-    {
-        "sort": 4,
-        "max_salaryfg": 200,
-        "min_salaryfasd": 130,
-    },
+    {"max_salary": 400, "min_salary": 110, "date_posted": 2},
+    {"max_salary": 300, "min_salary": 120, "date_posted": 3},
+    {"max_salary": 500, "min_salary": 100, "date_posted": 1},
+    {"max_sal": 200, "min_sal": 130, "date_post": 5},
+    {"max_salary": 200, "min_salary": 130, "date_posted": 4},
+]
+
+correctReturn = [
+    {"max_salary": 500, "min_salary": 100, "date_posted": 1},
+    {"max_salary": 400, "min_salary": 110, "date_posted": 2},
+    {"max_salary": 300, "min_salary": 120, "date_posted": 3},
+    {"max_salary": 200, "min_salary": 130, "date_posted": 4},
+    {"max_sal": 200, "min_sal": 130, "date_post": 5},
 ]
 
 
 def test_sort_by_criteria():
-    assert sort_by(jobs, criteria[0]) is jobs
-    assert sort_by(jobs, criteria[1]) is jobs
+    with pytest.raises(
+        ValueError, match=f"invalid sorting criteria: {criteria[-1]}"
+    ):
+        sort_by(jobs, criteria[-1])
