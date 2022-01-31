636b8050f83088af483373b94345b1e1df997afd
Bruna Campos <92brunacampos@gmail.com>
2022-01-30 22:17:22 -0300

    requisito 5

--- src/insights.py
+++ src/insights.py
@@ -122,7 +122,14 @@ def get_min_salary(path):
     int
         The minimum salary paid out of all job opportunities
     """
-    pass
+    jobs = read(path)
+
+    min_salary = 20000
+    for job in jobs:
+        if job["min_salary"].isnumeric():
+            if int(job["min_salary"]) < min_salary:
+                min_salary = int(job["min_salary"])
+    return min_salary
 
 
 def matches_salary_range(job, salary):
