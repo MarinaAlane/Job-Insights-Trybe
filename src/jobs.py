f4d508aa9dfc4722eb3b52b8f02fb4a1f39c43b9
Junior Portela <portelajr14@gmail.com>
2022-01-31 20:46:05 -0300

    feat: req1 - funcao read le arquivo csv e tranforma em dict

--- src/jobs.py
+++ src/jobs.py
@@ -9,26 +9,9 @@ def read(path):
         jobs_list = []
         for row in jobs_reader:
             new_job = {}
-            new_job["job_title"] = row["job_title"]
-            new_job["company"] = row["company"]
-            new_job["state"] = row["state"]
-            new_job["city"] = row["city"]
-            new_job["min_salary"] = row["min_salary"]
-            new_job["max_salary"] = row["max_salary"]
-            new_job["job_desc"] = row["job_desc"]
-            new_job["industry"] = row["industry"]
-            new_job["rating"] = row["rating"]
-            new_job["date_posted"] = row["date_posted"]
-            new_job["valid_until"] = row["valid_until"]
-            new_job["job_type"] = row["job_type"]
-            new_job["id"] = row["id"]
+            for key, value in row.items():
+                new_job[key] = value
             jobs_list.append(new_job)
         return jobs_list
 
-# new_title["job_title"] = row["job_title"]
-# company,state,city,min_salary,max_salary,job_desc,industry,rating,date_posted,valid_until,job_type,id
-# new_job = {}
-# new_job["job_title"] = row["job_title"]
-# new_job["company"] = row["company"]
-# new_job["state"] = row["state"]
-# new_job[]
+# read("src/jobs.csv")
