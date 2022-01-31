1c0308472f38d737d73610846d33005bc2417819
rodolfowolff <rodolfoluizwolff@gmail.com>
2022-01-30 23:58:49 -0300

    feat: req.10.1

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,22 @@
-# from src.sorting import sort_by
+import pytest
+from src.sorting import sort_by
 
 
-def test_sort_by_criteria():
-    pass
+@pytest.fixture
+def jobs():
+    return [
+        {"min_salary": 500, "max_salary": 10000, "date_posted": "2022-06-27"},
+        {"min_salary": 1500, "max_salary": 25000, "date_posted": "2022-09-13"},
+        {"min_salary": 1000, "max_salary": 15000, "date_posted": "2022-07-05"},
+    ]
+
+
+def test_sort_by_criteria(jobs):
+    order_by_min_salary = [
+        {"min_salary": 500, "max_salary": 10000, "date_posted": "2022-06-27"},
+        {"min_salary": 1000, "max_salary": 15000, "date_posted": "2022-07-05"},
+        {"min_salary": 1500, "max_salary": 25000, "date_posted": "2022-09-13"},
+    ]
+    jobs_min = jobs.copy()
+    sort_by(jobs_min, "min_salary")
+    assert jobs_min == order_by_min_salary
