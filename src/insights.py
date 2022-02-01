f69a7fd9851b8efff51065ec0e900cd41b22252d
deltadigo <deltadigo@gmail.com>
2022-02-01 19:39:26 -0300

    feat: requisito 3

--- src/insights.py
+++ src/insights.py
@@ -35,20 +35,21 @@ def filter_by_job_type(jobs, job_type):
 
 def get_unique_industries(path):
     """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
     """
-    return []
+
+    jobs = read(path)
+
+    industries = set()
+
+    for job in jobs:
+        industry = job["industry"]
+        if (industry != ""):
+            industries.add(industry)
+
+    return industries
+
+
+get_unique_industries("src/jobs.csv")
 
 
 def filter_by_industry(jobs, industry):
