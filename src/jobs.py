1eddca03cf39b0ed80d384f034b0689649625a2e
Carlos Henrique <henrique.joaovictor@hotmail.com>
2022-02-03 22:09:04 -0300

    done requirement read

--- src/jobs.py
+++ src/jobs.py
@@ -1,4 +1,5 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
@@ -15,4 +16,15 @@ def read(path):
     list
         List of rows as dicts
     """
-    return []
+    content = []
+
+    with open(path, "r") as file:
+        reader = csv.reader(file, delimiter=",", quotechar='"')
+        header, *data = reader
+
+        content = [
+            {header[index]: row[index] for index, _ in enumerate(row)}
+            for row in data
+        ]
+
+    return content
