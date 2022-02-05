1af7849c7c5726433ac3462ea838911780bd8e9a
Rafael Javaro <rafaeljavaro750@gmail.com>
2022-02-05 16:01:24 -0300

    Req 10 - Refatora deixando parametos ordenados

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -2,27 +2,90 @@ import pytest
 from src.sorting import sort_by
 
 jobs = [
-    {"max_salary": 4000, "min_salary": 2500, "date_posted": "02-05-2022"},
-    {"max_salary": 6000, "min_salary": 4500, "date_posted": "02-06-2022"},
-    {"max_salary": 8000, "min_salary": 6500, "date_posted": "02-07-2022"},
+    {
+        "max_salary": "6000",
+        "min_salary": "4500",
+        "date_posted": "04/10/2022"
+    },
+    {
+        "max_salary": "8000",
+        "min_salary": "6500",
+        "date_posted": "02/07/2021"
+    },
+    {
+        "max_salary": "10000",
+        "min_salary": "8500",
+        "date_posted": "05/11/2022"
+    },
 ]
 
-mock = {
-    "max_salary": [jobs],
-    "min_salary": [jobs],
-    "date_posted": [jobs],
+
+mocks = {
+    "max_salary": [
+        {
+            'max_salary': '10000',
+            'min_salary': '8500',
+            'date_posted': '05/11/2022'
+        },
+        {
+            'max_salary': '8000',
+            'min_salary': '6500',
+            'date_posted': '02/07/2021'
+        },
+        {
+            'max_salary': '6000',
+            'min_salary': '4500',
+            'date_posted': '04/10/2022'
+        }
+    ],
+    "min_salary": [
+        {
+            'max_salary': '6000',
+            'min_salary': '4500',
+            'date_posted': '04/10/2022'
+        },
+        {
+            'max_salary': '8000',
+            'min_salary': '6500',
+            'date_posted': '02/07/2021'
+        },
+        {
+            'max_salary': '10000',
+            'min_salary': '8500',
+            'date_posted': '05/11/2022'
+        }
+    ],
+    "date_posted": [
+        {
+            'max_salary': '6000',
+            'min_salary': '4500',
+            'date_posted': '04/10/2022'
+        },
+        {
+            'max_salary': '8000',
+            'min_salary': '6500',
+            'date_posted': '02/07/2021'
+        },
+        {
+            'max_salary': '10000',
+            'min_salary': '8500',
+            'date_posted': '05/11/2022'
+        }
+    ]
 }
 
 
 def test_sort_by_criteria():
-    sort_by(jobs, 'max_salary')
-    assert(jobs) == mock['max_salary']
+    sort_by(jobs, "max_salary")
+    assert (jobs) == mocks["max_salary"]
 
-    sort_by(jobs, 'min_salary')
-    assert(jobs) == mock['min_salary']
+    sort_by(jobs, "min_salary")
+    assert (jobs) == mocks["min_salary"]
 
-    sort_by(jobs, 'date_posted')
-    assert(jobs) == mock['date_posted']
+    sort_by(jobs, "date_posted")
+    assert (jobs) == mocks["date_posted"]
 
-    with pytest.raises(ValueError, 'invalid_criteria'):
-        sort_by(jobs, 'invalid_criteria')
+    with pytest.raises(
+        ValueError, match="invalid_criteria"
+    ):
+        sort_by(jobs, "invalid_criteria")
