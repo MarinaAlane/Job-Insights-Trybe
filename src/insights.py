1fe7d394ace1dc2d5468f4198a5bbb8e21ca300b
Rafael Javaro <rafaeljavaro750@gmail.com>
2022-02-05 14:27:04 -0300

    Req 7

--- src/insights.py
+++ src/insights.py
@@ -28,21 +28,8 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    filtered_by_industry = [r for r in jobs if r['industry'] == industry]
+    return filtered_by_industry
 
 
 def get_max_salary(path):
