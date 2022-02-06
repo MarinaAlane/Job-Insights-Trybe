d98007c7ebdaba17b15430d18fefa0145f8932bf
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2022-02-06 18:47:57 -0300

    Requisito 6

--- src/insights.py
+++ src/insights.py
@@ -24,7 +24,13 @@ def filter_by_job_type(jobs, job_type):
     list
         List of jobs with provided job_type
     """
-    return []
+    Alljobs = list()
+
+    for job in jobs:
+        if job["job_type"] == job_type
+            Alljobs.append(job)
+            
+    return Alljobs
 
 
 def get_unique_industries(path):
