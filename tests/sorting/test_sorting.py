830ee7cc5c95967bf822cf4ce513bb59761ec2ad
GabriellCastro <kastro.gabriel17@gmail.com>
2022-02-06 20:54:29 -0400

    refatorando: funçao test_sorting!  🚀

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,49 +1,38 @@
-import pytest
 from src.sorting import sort_by
 
 
-JOB_1 = {
-    "min_salary": 150,
-    "max_salary": 1500,
-    "date_posted": "2021/02/01",
-}
-
-JOB_2 = {
-    "min_salary": 250,
-    "max_salary": 1000,
-    "date_posted": "2021/03/01",
-}
-
-JOB_3 = {
-    "min_salary": 350,
-    "max_salary": 2000,
-    "date_posted": "2021/01/01",
-}
-
-
 def test_sort_by_criteria():
-    jobs = [JOB_1, JOB_2, JOB_3]
-
-    sorted_by_min_salary = [
-        JOB_1,
-        JOB_2,
-        JOB_3,
+    jobs = [
+        {'min_salary': '', 'max_salary': '', 'date_posted': ''},
+        {'min_salary': 5, 'max_salary': 10, 'date_posted': '2018-01-01'},
+        {'min_salary': 0, 'max_salary': 20, 'date_posted': '2016-01-01'},
+        {'min_salary': -50, 'max_salary': 30, 'date_posted': '2015-01-01'},
+        {'min_salary': 100, 'max_salary': 200, 'date_posted': '2021-01-01'},
     ]
 
-    sorted_by_max_salary = [
-        JOB_3,
-        JOB_1,
-        JOB_2,
+    sort_by(jobs, 'min_salary')
+    assert jobs == [
+        {'min_salary': -50, 'max_salary': 30, 'date_posted': '2015-01-01'},
+        {'min_salary': 0, 'max_salary': 20, 'date_posted': '2016-01-01'},
+        {'min_salary': 5, 'max_salary': 10, 'date_posted': '2018-01-01'},
+        {'min_salary': 100, 'max_salary': 200, 'date_posted': '2021-01-01'},
+        {'min_salary': '', 'max_salary': '', 'date_posted': ''},
     ]
 
-    sorted_by_date_posted = [
-        JOB_2,
-        JOB_1,
-        JOB_3,
+    sort_by(jobs, 'max_salary')
+    assert jobs == [
+        {'min_salary': 100, 'max_salary': 200, 'date_posted': '2021-01-01'},
+        {'min_salary': -50, 'max_salary': 30, 'date_posted': '2015-01-01'},
+        {'min_salary': 0, 'max_salary': 20, 'date_posted': '2016-01-01'},
+        {'min_salary': 5, 'max_salary': 10, 'date_posted': '2018-01-01'},
+        {'min_salary': '', 'max_salary': '', 'date_posted': ''},
     ]
 
-    assert sort_by(jobs, "min_salary") is sorted_by_min_salary
-    assert sort_by(jobs, "max_salary") is sorted_by_max_salary
-    assert sort_by(jobs, "date_posted") is sorted_by_date_posted
-    with pytest.raises(AssertionError):
-        sort_by(jobs, "ulaula")
+    sort_by(jobs, 'date_posted')
+    assert jobs == [
+        {'min_salary': 100, 'max_salary': 200, 'date_posted': '2021-01-01'},
+        {'min_salary': 5, 'max_salary': 10, 'date_posted': '2018-01-01'},
+        {'min_salary': 0, 'max_salary': 20, 'date_posted': '2016-01-01'},
+        {'min_salary': -50, 'max_salary': 30, 'date_posted': '2015-01-01'},
+        {'min_salary': '', 'max_salary': '', 'date_posted': ''},
+    ]
