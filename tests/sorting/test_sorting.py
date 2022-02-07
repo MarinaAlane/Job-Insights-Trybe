ea47947fc622b121448b13cdd86c79f20821a0d4
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2022-02-07 01:32:00 -0300

    feat: fix req10[8]

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -56,21 +56,21 @@ mock = {
         },
         ],
     "date_posted": [
-        {
-            "min_salary": 900,
-            "max_salary": 4800,
-            "date_posted": "2022-03-12"
-        },
+       {
+            "min_salary": 1100,
+            "max_salary": 5200,
+            "date_posted": "2022-03-14"
+        }
         {
             "min_salary": 1000,
             "max_salary": 5000,
             "date_posted": "2022-03-13"
         },
         {
-            "min_salary": 1100,
-            "max_salary": 5200,
-            "date_posted": "2022-03-14"
-        }
+            "min_salary": 900,
+            "max_salary": 4800,
+            "date_posted": "2022-03-12"
+        },
         ],
 }
 
