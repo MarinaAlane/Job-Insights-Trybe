9c8285c0e3d58800976f6c9e7e7f7acd8cf33998
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2022-02-07 00:36:42 -0300

    Fix: reescrevendo Req10 - testes

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -18,46 +18,20 @@ jobs = [
     }
 ]
 
+mock = [
+    "min_salary": [jobs]
+    "max_salary": [jobs]
+    "date_posted": [jobs]
+]
+
 
 def test_sort_by_criteria():
-    job_list_min_salary_mock = [
-        {
-            "min_salary": 900,
-            "max_salary": 4800,
-            "date_posted": "2022-03-12"
-        },
-        {
-            "min_salary": 1000,
-            "max_salary": 5000,
-            "date_posted": "2022-03-13"
-        },
-        {
-            "min_salary": 1100,
-            "max_salary": 5200,
-            "date_posted": "2022-03-14"
-        }
-    ]
-
-    job_list_date_posted_mock = [
-        {
-            "min_salary": 900,
-            "max_salary": 4800,
-            "date_posted": "2022-03-12"
-        },
-        {
-            "min_salary": 1000,
-            "max_salary": 5000,
-            "date_posted": "2022-03-13"
-        },
-        {
-            "min_salary": 1100,
-            "max_salary": 5200,
-            "date_posted": "2022-03-14"
-        }
-    ]
-
+    
     sort_by(jobs, "min_salary")
-    assert(jobs) == job_list_min_salary_mock
+    assert jobs == mock["min_salary"]
+
+    sort_by(jobs, "max_salary")
+    assert jobs == mock["max_salary"]
 
     sort_by(jobs, "date_posted")
-    assert(jobs) == job_list_date_posted_mock
+    assert jobs == mock["date_posted"]
