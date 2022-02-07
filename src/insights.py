2e447472d9cd44a89c0d1bc2f1aec21dff7c47b2
Junior Portela <portelajr14@gmail.com>
2022-02-07 12:54:33 -0300

    feat: req8 - funcao retorna menor salario da lista

--- src/insights.py
+++ src/insights.py
@@ -40,21 +40,15 @@ def get_max_salary(path):
 
 
 def get_min_salary(path):
-    """Get the minimum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    pass
+    data_jobs = read(path)
+    min_salary = min(
+        [
+            int(job["min_salary"])
+            for job in data_jobs
+            if job["min_salary"].isnumeric() is True
+        ]
+    )
+    return min_salary
 
 
 def matches_salary_range(job, salary):
