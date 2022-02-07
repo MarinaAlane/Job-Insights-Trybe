dc536358b608a7c70c1c7e72c517a7163125480c
IgorMendez <igor.emp@hotmail.com>
2022-02-06 22:51:59 -0300

    req04

--- src/insights.py
+++ src/insights.py
@@ -59,21 +59,13 @@ def filter_by_industry(jobs, industry):
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
+    salaries = read(path)
+    max_salary = set()
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    pass
+    for salary in salaries:
+        if salary['max_salary'] != 'invalid' and salary['max_salary'] != '':
+            max_salary.add(int(salary['max_salary']))
+    return max(max_salary)
 
 
 def get_min_salary(path):
