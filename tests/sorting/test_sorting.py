e4cecc6b8e714971bb162302dc23a3e179eceefe
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2022-02-07 00:04:16 -0300

    Fix: identacao

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -4,13 +4,13 @@ jobs = [
     {
         "min_salary": "900",
         "max_salary": "4800",
-        "date_posted": "2022-03-12" 
-    }
+        "date_posted": "2022-03-12"
+    },
     {
         "min_salary": "1000",
         "max_salary": "5000",
         "date_posted": "2022-03-13"
-    }
+    },
     {
         "min_salary": "1100",
         "max_salary": "5200",
@@ -18,18 +18,43 @@ jobs = [
     }
 ]
 
+
 def test_sort_by_criteria():
     pass
     job_list_min_salary_mock = [
-        {"min_salary": "900", "max_salary": "4800", "date_posted": "2022-03-12"},
-        {"min_salary": "1000", "max_salary": "5000", "date_posted": "2022-03-13"},
-        {"min_salary": "1100", "max_salary": "5200", "date_posted": "2022-03-14"}
+        {
+            "min_salary": "900",
+            "max_salary": "4800",
+            "date_posted": "2022-03-12"
+        },
+        {
+            "min_salary": "1000",
+            "max_salary": "5000",
+            "date_posted": "2022-03-13"
+        },
+        {
+            "min_salary": "1100",
+            "max_salary": "5200",
+            "date_posted": "2022-03-14"
+        }
     ]
 
     job_list_date_posted_mock = [
-        {"min_salary": "900", "max_salary": "4800", "date_posted": "2022-03-12"},
-        {"min_salary": "1000", "max_salary": "5000", "date_posted": "2022-03-13"},
-        {"min_salary": "1100", "max_salary": "5200", "date_posted": "2022-03-14"}
+        {
+            "min_salary": "900",
+            "max_salary": "4800",
+            "date_posted": "2022-03-12"
+        },
+        {
+            "min_salary": "1000",
+            "max_salary": "5000",
+            "date_posted": "2022-03-13"
+        },
+        {
+            "min_salary": "1100",
+            "max_salary": "5200",
+            "date_posted": "2022-03-14"
+        }
     ]
 
     sort_by(jobs, "min_salary")
