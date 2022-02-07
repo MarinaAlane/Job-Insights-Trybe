93ce179b52fef65cec816764c2a61e78d3e15955
IgorMendez <igor.emp@hotmail.com>
2022-02-06 22:56:05 -0300

    req07

--- src/insights.py
+++ src/insights.py
@@ -28,21 +28,8 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    filter_industry = [industry for industry in jobs if industry['industry'] == industry]
+    return filter_industry
 
 
 def get_max_salary(path):
