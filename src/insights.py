aeb3713e11b0b13b62da0757880f69779dd7277e
Junior Portela <portelajr14@gmail.com>
2022-02-07 13:19:16 -0300

    feat: reqs 8 e 9 - funcoes verificam erro em entradas e filtra jobs por salario em insights.py

--- src/insights.py
+++ src/insights.py
@@ -52,47 +52,28 @@ def get_min_salary(path):
 
 
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
+    if "min_salary" not in job or "max_salary" not in job:
+        raise ValueError()
+    if (
+        type(job["min_salary"]) is not int or
+        type(job["max_salary"]) is not int or
+        type(salary) is not int
+    ):
+        raise ValueError()
+    if (job["min_salary"] > job["max_salary"]):
+        raise ValueError()
+    return job["min_salary"] <= salary <= job["max_salary"]
 
 
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    return []
+    filtered_salary = []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                filtered_salary.append(job)
+        except ValueError:
+            pass
+    return filtered_salary
 
 
 # https://learncodeimprove.com/python/list-of-unique-items-comprehension/
