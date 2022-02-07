16013d6faa7d246b68522a1e0bc9d70b95dc4e61
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2022-02-07 01:26:14 -0300

    feat: fix req10[7]

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -21,9 +21,57 @@ jobs = [
 ]
 
 mock = {
-    "min_salary": [jobs],
-    "max_salary": [jobs],
-    "date_posted": [jobs]
+    "min_salary": [
+        {
+            "min_salary": 900,
+            "max_salary": 4800,
+            "date_posted": "2022-03-12"
+        },
+        {
+            "min_salary": 1000,
+            "max_salary": 5000,
+            "date_posted": "2022-03-13"
+        },
+        {
+            "min_salary": 1100,
+            "max_salary": 5200,
+            "date_posted": "2022-03-14"
+        },
+        ],
+    "max_salary": [
+        {
+            "min_salary": 1100,
+            "max_salary": 5200,
+            "date_posted": "2022-03-14"
+        },
+        {
+            "min_salary": 1000,
+            "max_salary": 5000,
+            "date_posted": "2022-03-13"
+        },
+        {
+            "min_salary": 900,
+            "max_salary": 4800,
+            "date_posted": "2022-03-12"
+        },
+        ],
+    "date_posted": [
+        {
+            "min_salary": 900,
+            "max_salary": 4800,
+            "date_posted": "2022-03-12"
+        },
+        {
+            "min_salary": 1000,
+            "max_salary": 5000,
+            "date_posted": "2022-03-13"
+        },
+        {
+            "min_salary": 1100,
+            "max_salary": 5200,
+            "date_posted": "2022-03-14"
+        }
+        ],
 }
 
 
