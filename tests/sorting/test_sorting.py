cbd8f87e5d05ae61db456884d2dd29173fd3130c
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2022-02-07 01:33:44 -0300

    fix: lint req10[2]

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -60,7 +60,7 @@ mock = {
             "min_salary": 1100,
             "max_salary": 5200,
             "date_posted": "2022-03-14"
-        }
+        },
         {
             "min_salary": 1000,
             "max_salary": 5000,
@@ -85,6 +85,3 @@ def test_sort_by_criteria():
 
     sort_by(jobs, "date_posted")
     assert jobs == mock["date_posted"]
-
-    with pytest.raises(ValueError, "wrong_criteria"):
-        sort_by(jobs, "wrong_criteria")
