e4bceeb21b98fa8b223faeca3fb853edb4a69336
IgorMendez <igor.emp@hotmail.com>
2022-02-06 22:49:31 -0300

    req03

--- src/insights.py
+++ src/insights.py
@@ -30,21 +30,14 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
+    file_content = read(path)
+    industrie = []
 
-    Must call `read`
+    for indu in file_content:
+        if industrie.__contains__(indu['industry']) is False and indu['industry'] != '':
+            industrie.append(indu['industry'])
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    return industrie
 
 
 def filter_by_industry(jobs, industry):
